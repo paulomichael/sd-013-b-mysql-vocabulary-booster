2337339ada6f2d66a0df38db5e4b226f08369bc9
Rodrigo Augusto <rodrigoaugusto96@outlook.com>
2021-10-24 09:42:58 -0300

    Requisito 13

---
+++ desafio13.sql
@@ -0,0 +1,10 @@
+SELECT
+	product.productName AS 'Produto',
+    product.price AS 'Price'
+FROM
+	w3schools.products AS product
+WHERE product.productID
+	IN (SELECT orders.productID
+    FROM w3schools.order_details AS orders
+    WHERE orders.quantity > 80)
+ORDER BY Produto ASC;
