35c06bff4998a08511af4d50281be6b282ba1b90
matheuscarvalhoscm <matheuscarvalhoscm@gmail.com>
2021-10-25 16:33:14 -0300

    Corrige requisito 6 (???)

--- desafio6.sql
+++ desafio6.sql
@@ -7,7 +7,7 @@ FROM
   hr.job_history AS JH
   INNER JOIN hr.employees AS E ON JH.EMPLOYEE_ID = E.EMPLOYEE_ID
   INNER JOIN hr.jobs AS J ON J.JOB_ID = JH.JOB_ID
-  INNER JOIN hr.departments AS D ON D.DEPARTMENT_ID = E.DEPARTMENT_ID
+  INNER JOIN hr.departments AS D ON D.DEPARTMENT_ID = JH.DEPARTMENT_ID
 ORDER BY
   `Nome Completo` DESC,
   `Cargo` ASC;
