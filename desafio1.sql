82a6a05cd42d50300df7c4e6b5d6ff52f690dcb4
Henrique <henriquecursino46@gmail.com>
2021-10-25 15:35:45 -0300

    Erro no code climate

--- desafio1.sql
+++ desafio1.sql
@@ -1,8 +1,8 @@
-SELECT
-	COUNTRY_NAME AS 'País',
-  IF(REGION_ID = 1,
-	  'incluído',
-    'não inscluído') AS 'Status Inclusão'
+SELECT 
+    country_name AS 'País',
+    IF(region_id = 1,
+        'incluído',
+        'não incluído') AS 'Status Inclusão'
 FROM
-	hr.countries
+    hr.countries
 ORDER BY COUNTRY_NAME;
