a73bcd6609e59a84b76147e175a660838be934d6
Guilherme da Costa <guilhermedc93@gmail.com>
2021-10-25 14:44:23 -0300

    conclui desafio 10 finalizado 17/10/21

---
+++ desafio10.sql
@@ -0,0 +1,11 @@
+SELECT
+	P.ProductName AS `Produto`,
+    MIN(O.Quantity) AS `Mínima`,
+	MAX(O.Quantity) AS `Máxima`,
+    ROUND(AVG(O.Quantity), 2) AS `Média`       
+FROM w3schools.order_details AS O
+JOIN w3schools.products AS P
+ON P.ProductID = O.ProductID
+GROUP BY P.ProductName
+HAVING `Média` > 20
+ORDER BY `Média`, `Produto`;
