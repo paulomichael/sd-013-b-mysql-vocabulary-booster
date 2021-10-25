7d1ad7dfdd5b9635474ab8606a03a958f7382d14
Leonardo <leo_mc_inter@hotmail.com>
2021-10-25 19:48:42 -0300

    Requisito 6

--- desafio6.sql
+++ desafio6.sql
@@ -10,5 +10,5 @@ FROM
         JOIN
     hr.jobs AS j ON j.JOB_ID = jh.JOB_ID
         JOIN
-    hr.departments AS d ON d.DEPARTMENT_ID = e.DEPARTMENT_ID
+    hr.departments AS d ON d.DEPARTMENT_ID = jh.DEPARTMENT_ID
 ORDER BY `Nome completo` DESC , `Cargo`;
