d85df14911e7cdb96e7b381b30aee47c21e75633
Alef Sloan <Alef_sloan@hotmail.com>
2021-10-25 19:22:04 -0300

    Corrigindo erros no requisito 8

--- desafio8.sql
+++ desafio8.sql
@@ -1,6 +1,6 @@
 SELECT ContactName AS `Nome de contato`,
 ShipperName AS `Empresa que fez o envio`,
-OrderDate AS `Data do envio`
+OrderDate AS `Data do pedido`
 FROM w3schools.order_details AS order_details
 INNER JOIN w3schools.orders AS orders ON orders.OrderID = order_details.OrderID
 INNER JOIN w3schools.shippers AS shippers ON shippers.ShipperID = orders.ShipperID
