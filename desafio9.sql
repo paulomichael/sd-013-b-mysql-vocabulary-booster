f208adde6419c363b339278af1ee502989f1731e
marcusrodriguesdev <marcus.rodrigues10@hotmail.com>
2021-10-25 20:23:33 -0300

    solve 8 and 9

--- desafio9.sql
+++ desafio9.sql
@@ -1,9 +1,9 @@
 SELECT 
-    CONCAT(E.FirstName, ' ', LastName) AS 'Nome Completo',
+    CONCAT(E.FirstName, ' ', E.LastName) AS 'Nome Completo',
     COUNT(O.EmployeeId) AS 'Total de pedidos'
 FROM
     w3schools.employees AS E
-        INNER JOIN
-    w3schools.orders AS O ON O.employeeID = E.employeeID
+        JOIN
+    w3schools.orders AS O ON E.employeeID = O.employeeID
 GROUP BY `Nome Completo`
 ORDER BY `Total de pedidos`;
