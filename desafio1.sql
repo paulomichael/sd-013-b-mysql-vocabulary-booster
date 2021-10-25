423c5f36b97dcd847aff6486ec64af06cd4a7217
Vinicius <vinicius.10.carvalho.100@gmail.com>
2021-10-25 14:36:07 -0300

    Projeto MySQL Vocabulary Booster

---
+++ desafio1.sql
@@ -0,0 +1,6 @@
+SELECT c.COUNTRY_NAME AS País, 
+CASE 
+	WHEN r.REGION_NAME = 'Europe' THEN 'incluído' 
+	ELSE 'não incluído'
+END AS 'Status Inclusão'
+FROM hr.countries AS c INNER JOIN hr.regions AS r ON c.REGION_ID = r.REGION_ID ORDER BY c.COUNTRY_NAME;
