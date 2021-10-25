f7364ea2c74e1e429c13769b2a57114fd6a68fe5
Ygor Lage <75700712+Yg-or@users.noreply.github.com>
2021-10-25 18:55:50 -0300

    requisito 6

--- desafio6.sql
+++ desafio6.sql
@@ -1,7 +1,7 @@
 SELECT CONCAT(FIRST_NAME, ' ', LAST_NAME) AS `Nome completo`,
 empregos.JOB_TITLE AS `Cargo`,
-HIRE_DATE,
-DEPARTMENT_NAME
+HIRE_DATE AS `Data de início do cargo`,
+DEPARTMENT_NAME AS `Departamento`
 FROM hr.employees AS employee
 INNER JOIN hr.jobs as empregos
 ON employee.JOB_ID = empregos.JOB_ID
