fc08c9427dff92da4c6c4942a3520f6b5bdc9656
Amós Rodrigues <amos.adm.rh@gmail.com>
2021-10-25 14:53:19 -0300

    realiza requisito 1

--- desafio1.sql
+++ desafio1.sql
@@ -1,2 +1,10 @@
-SELECT COUNT(DISTINCT job_id)
-FROM hr.employees;
+SELECT * FROM hr.regions WHERE REGION_NAME = "Europe";
+SELECT * FROM hr.countries;
+
+SELECT 
+	COUNTRY_NAME AS 'País',
+    CASE
+		WHEN REGION_ID IN(SELECT REGION_ID FROM hr.regions WHERE REGION_NAME = "Europe") THEN 'incluído'
+        ELSE 'não incluído'
+        END AS 'Status Inclusão'
+FROM hr.countries;
