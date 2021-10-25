ab5cf23d59f8d5df98fa0787887470c7f5a12f0c
Felipe Ventorim <felipeventorim11@gmail.com>
2021-10-25 15:52:45 -0300

    Desafio7

---
+++ desafio7.sql
@@ -0,0 +1,7 @@
+SELECT UCASE(CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME)) AS `Nome completo`,
+jh.START_DATE AS `Data de início`,
+e.SALARY AS `Salário`
+FROM hr.employees AS e
+JOIN job_history AS jh ON jh.EMPLOYEE_ID = e.EMPLOYEE_ID
+WHERE MONTH(jh.START_DATE) BETWEEN 1 AND 3
+ORDER BY `Nome completo`, `Data de início`;
