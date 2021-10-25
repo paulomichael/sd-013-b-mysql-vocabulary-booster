3e303985e51922319917c6513489d2c14710b54b
Amós Rodrigues <amos.adm.rh@gmail.com>
2021-10-25 15:22:34 -0300

    refatora requisito para atender code climate

--- desafio1.sql
+++ desafio1.sql
@@ -1,7 +1,4 @@
 SELECT 
 	COUNTRY_NAME AS 'País',
-    CASE
-		WHEN REGION_ID IN(SELECT REGION_ID FROM hr.regions WHERE REGION_NAME = "Europe") THEN 'incluído'
-        ELSE 'não incluído'
-        END AS 'Status Inclusão'
+    IF(REGION_ID IN(SELECT REGION_ID FROM hr.regions WHERE REGION_NAME = "Europe"), 'incluído', 'não incluído') AS 'Status Inclusão'
 FROM hr.countries ORDER BY COUNTRY_NAME;
