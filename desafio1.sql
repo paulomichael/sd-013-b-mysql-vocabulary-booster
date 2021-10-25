de1e7b2651ae4d03b4103aede88472e5509e0861
mariananbp <mariananbp@gmail.com>
2021-10-25 19:23:02 -0300

    Refactoring challenge 1

--- desafio1.sql
+++ desafio1.sql
@@ -1,7 +1,5 @@
 SELECT COUNTRY_NAME AS `País`,
-CASE
-WHEN REGION_ID = 1 THEN 'incluído'
-    ELSE 'não incluído'
-END AS `Status Inclusão`
+IF(REGION_ID = 1, 'incluído', 'não incluído')
+AS `Status Inclusão`
 FROM hr.countries
 ORDER BY COUNTRY_NAME;
