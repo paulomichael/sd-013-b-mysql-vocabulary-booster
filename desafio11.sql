21267ee0f704174af584337b311949d4019344d0
matheuscarvalhoscm <matheuscarvalhoscm@gmail.com>
2021-10-25 17:45:19 -0300

    code climate

--- desafio11.sql
+++ desafio11.sql
@@ -1,7 +1,7 @@
 SELECT
-	C1.ContactName AS `Nome`,
-    C1.Country AS `País`,
-    COUNT(C1.Country) - 1 AS `Número de compatriotas`
+  C1.ContactName AS `Nome`,
+  C1.Country AS `País`,
+  COUNT(C1.Country) - 1 AS `Número de compatriotas`
 FROM 
   w3schools.customers AS C1,
   w3schools.customers AS C2
