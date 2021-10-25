bf134742f677caa4abeea0e8da32ccff23ed596e
Rafael Nery Machado <rafaelnerymachado@gmail.com>
2021-10-25 16:23:48 -0300

    requisito 1 - corrigido erro de digitaçao

--- desafio1.sql
+++ desafio1.sql
@@ -2,7 +2,7 @@ SELECT
     COUNTRY_NAME AS País,
     IF(REGION_ID = 1,
         'incluído',
-        'não incluido') AS `Status Inclusão`
+        'não incluído') AS `Status Inclusão`
 FROM
     hr.countries
 ORDER BY COUNTRY_NAME;
