04ed55fc418a7ee8478bbf187799da5869685009
Natalia Camarinha <natcamarinha@gmail.com>
2021-10-25 16:48:23 -0300

    correção codeclimate requisitos 4, 5 e 6

--- desafio6.sql
+++ desafio6.sql
@@ -1,4 +1,5 @@
-SELECT CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) AS 'Nome Completo',
+SELECT 
+  CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) AS 'Nome Completo',
 	j.JOB_TITLE AS 'Cargo',
   e.HIRE_DATE AS 'Data de início do cargo',
   d.DEPARTMENT_NAME AS 'Departamento'
