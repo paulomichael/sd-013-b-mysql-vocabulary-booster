e10848ef99117defbb43df341001d718179dc857
luis <luizcruz0809@gmail.com>
2021-10-25 17:36:59 -0300

    test

--- desafio5.sql
+++ desafio5.sql
@@ -1,11 +1,10 @@
 SELECT
-		J.JOB_TITLE Cargo,
-		(J.MAX_SALARY - J.MIN_SALARY) `Variação Salarial`,
-		ROUND(J.MIN_SALARY / 12, 2) `Média mínima mensal`,
-		ROUND(J.MAX_SALARY / 12, 2) `Média máxima mensal`
+J.JOB_TITLE Cargo,
+(J.MAX_SALARY - J.MIN_SALARY) `Variação Salarial`,
+ROUND(J.MIN_SALARY / 12, 2) `Média mínima mensal`,
+ROUND(J.MAX_SALARY / 12, 2) `Média máxima mensal`
 FROM jobs J
 INNER JOIN employees E
 ON J.JOB_ID = E.JOB_ID
 GROUP BY J.JOB_ID
 ORDER BY `Variação Salarial`, Cargo;
-
