163eaa2c80cf2879ea238b7e6a2e38e1a6d72032
Alef Sloan <Alef_sloan@hotmail.com>
2021-10-25 18:10:17 -0300

    Corrigindo o Requisito 6 - erro na interpretaçao

--- desafio6.sql
+++ desafio6.sql
@@ -1,10 +1,9 @@
 SELECT CONCAT(employees.FIRST_NAME, ' ', employees.LAST_NAME) AS `Nome completo`,
 jobs.JOB_TITLE AS `Cargo`,
-employees.HIRE_DATE AS `Data de início do cargo`,
+job_history.START_DATE AS `Data de início do cargo`,
 department.DEPARTMENT_NAME AS `Departamento`
-FROM hr.employees AS employees
-INNER JOIN hr.jobs AS jobs
-ON jobs.JOB_ID = employees.JOB_ID
-INNER JOIN hr.departments AS department
-ON department.DEPARTMENT_ID = employees.DEPARTMENT_ID
+FROM hr.job_history AS job_history
+INNER JOIN hr.employees AS employees ON job_history.EMPLOYEE_ID = employees.EMPLOYEE_ID
+INNER JOIN hr.jobs AS jobs ON jobs.JOB_ID = job_history.JOB_ID
+INNER JOIN hr.departments AS department ON department.DEPARTMENT_ID = job_history.DEPARTMENT_ID
 ORDER BY `Nome completo` DESC, `Cargo` ASC;
