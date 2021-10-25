3ff77f6355ed65b45d280d4fddcbffc7849cd47b
Rafael Nery Machado <rafaelnerymachado@gmail.com>
2021-10-25 18:29:42 -0300

    requisito 11 - concluido

---
+++ desafio11.sql
@@ -0,0 +1,12 @@
+SELECT 
+    cus1.ContactName AS Nome,
+    cus1.Country AS País,
+    COUNT(cus1.Country) - 1 AS `Número de compatriotas`
+FROM
+    w3schools.customers AS cus1,
+    w3schools.customers AS cus2
+WHERE
+    cus1.Country = cus2.Country
+GROUP BY Nome
+HAVING `Número de compatriotas` > 0
+ORDER BY Nome;
