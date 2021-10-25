b9fef33cf8e147f360168e241dbecd7a504700d1
Julio Pereira <jandrade.pereira1@gmail.com>
2021-10-25 18:10:17 -0300

    Requisito 5 - retira o INNER JOIN e suas propriedades

--- desafio5.sql
+++ desafio5.sql
@@ -1,11 +1,6 @@
-SELECT 
-    jobs_table.JOB_TITLE AS `Cargo`,
-    jobs_table.MAX_SALARY - jobs_table.MIN_SALARY AS `Variação Salarial`,
-	  ROUND(AVG(jobs_table.MIN_SALARY/12), 2) AS `Média mínima mensal`,
-    ROUND(AVG(jobs_table.MAX_SALARY/12), 2) AS `Média máxima mensal`
-FROM
-    hr.employees AS employees_table
-        INNER JOIN
-    hr.jobs AS jobs_table ON employees_table.JOB_ID = jobs_table.JOB_ID
-GROUP BY jobs_table.JOB_TITLE
+SELECT JOB_TITLE AS `Cargo`,
+    MAX_SALARY - MIN_SALARY AS `Variação Salarial`,
+	  ROUND(AVG(MIN_SALARY/12), 2) AS `Média mínima mensal`,
+    ROUND(AVG(MAX_SALARY/12), 2) AS `Média máxima mensal`
+FROM hr.employees
 ORDER BY `Variação Salarial` ASC, `Cargo`;
