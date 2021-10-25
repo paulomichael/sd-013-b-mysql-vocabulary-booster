d3a7eb1e94021eb4d4a699161de1c82dc59bd181
Camila Dornas <camiladornas@gmail.com>
2021-10-25 16:11:43 -0300

    Agora vai

--- desafio3.sql
+++ desafio3.sql
@@ -3,5 +3,4 @@ SELECT
     MAX_SALARY - MIN_SALARY AS 'Diferença entre salários máximo e mínimo'
 FROM
     hr.jobs
-ORDER BY
-  `Diferença entre salários máximo e mínimo`, `Cargo`;
+ORDER BY `Diferença entre salários máximo e mínimo`, `Cargo`;
