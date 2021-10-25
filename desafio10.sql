9fff9ddd973a906ce58f7c79b1e5bab47bd1b6ce
Felipe Ventorim <felipeventorim11@gmail.com>
2021-10-25 16:42:55 -0300

    Desafio10

--- desafio10.sql
+++ desafio10.sql
@@ -1,9 +1,12 @@
-SELECT p.ProductName AS `Produto`,
-MIN(o.Quantity) AS `Mínima`,
-MAX(o.Quantity) AS `Máxima`,
-ROUND(AVG(o.Quantity), 2) AS `Média`
-FROM w3schools.products AS p
-JOIN order_details AS o ON o.ProductID = p.ProductID
+SELECT 
+    p.ProductName AS `Produto`,
+    MIN(o.Quantity) AS Mínima,
+    MAX(o.Quantity) AS Máxima,
+    ROUND(AVG(o.Quantity), 2) AS Média
+FROM
+    w3schools.products AS p
+        JOIN
+    order_details AS o ON o.ProductID = p.ProductID
 GROUP BY o.ProductID
-HAVING `Média` > 20
-ORDER BY `Média`, `Produto`;
+HAVING Média > 20
+ORDER BY Média , Produto;
