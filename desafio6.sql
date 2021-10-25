b0444e978c332da0c36b26ba662fe13ca5f83ce0
matheuscarvalhoscm <matheuscarvalhoscm@gmail.com>
2021-10-25 16:07:09 -0300

    corrige requisito 6

--- desafio6.sql
+++ desafio6.sql
@@ -1,7 +1,7 @@
 SELECT
   CONCAT(E.FIRST_NAME, ' ', E.LAST_NAME) AS `Nome Completo`,
   J.JOB_TITLE AS `Cargo`,
-  JH.START_DATE AS `Data de ínicio no cargo`,
+  JH.START_DATE AS `Data de início do cargo`,
   D.DEPARTMENT_NAME AS `Departamento`
 FROM
   hr.job_history AS JH
