c4019a17570cd7ac95d8714da80be92996f9ee58
enioandrade92 <enio.goulart@gmail.com>
2021-10-25 20:48:33 -0300

    Requisito 6

--- requisito4.sql
+++ requisito4.sql
@@ -1,6 +1,6 @@
 SELECT 
     j.JOB_TITLE AS `Cargo`,
-    ROUND(AVG(e.SALARY), 2) AS Média salarial,
+    ROUND(AVG(e.SALARY), 2) AS `Média salarial`,
     CASE
         WHEN
             ROUND(AVG(e.SALARY), 2) >= 2000
@@ -18,12 +18,12 @@ SELECT
         THEN
             'Pleno'
         ELSE 'CEO'
-    END AS Senioridade
+    END AS `Senioridade`
 FROM
     hr.jobs AS j
         JOIN
     hr.employees AS e ON j.JOB_ID = e.JOB_ID
-GROUP BY Cargo
-ORDER BY Média salarial , Cargo;
+GROUP BY `Cargo`
+ORDER BY `Média salarial` , `Cargo`;
 
 
---
+++ requisito6.sql
@@ -0,0 +1,14 @@
+SELECT
+    CONCAT(em.first_name, ' ', em.last_name) AS `Nome Completo`,
+    jo.job_title AS `Cargo`,
+    joh.START_DATE AS `Data de início do cargo`,
+    de.DEPARTMENT_NAME AS `Departamento`
+FROM
+    hr.employees AS em 
+	JOIN
+    hr.job_history AS joh ON em.employee_id = joh.employee_id
+    JOIN
+    hr.jobs AS jo ON em.job_id = jo.job_id
+	JOIN
+    hr.departments AS de ON joh.DEPARTMENT_ID = de.DEPARTMENT_ID
+ORDER BY `Nome Completo` DESC , `Data de início do cargo`;
