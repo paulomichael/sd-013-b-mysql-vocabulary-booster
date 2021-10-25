e245304612b0a5ee6550907fafcac2a0a5905706
Joao Victor Cristino F <jvictorfranca@yahoo.com.br>
2021-10-25 19:43:42 -0300

    tenta resolver o 6

--- desafio6.sql
+++ desafio6.sql
@@ -1,5 +1,4 @@
-USE hr;
-SELECT t2.FIRST_NAME + ' ' + t2.LAST_NAME AS "Nome completo", t3.JOB_TITLE AS 'Cargo', t1.START_DATE AS "Data de início do cargo", t4.DEPARTMENT_NAME AS "Departamento"
+SELECT CONCAT(t2.FIRST_NAME, ' ', t2.LAST_NAME) AS "Nome completo", t3.JOB_TITLE AS 'Cargo', t1.START_DATE AS "Data de início do cargo", t4.DEPARTMENT_NAME AS "Departamento"
 FROM job_history AS t1
 INNER JOIN employees as t2 
 ON t1.EMPLOYEE_ID = t2.EMPLOYEE_ID 
