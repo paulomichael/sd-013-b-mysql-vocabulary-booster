1e18d3a60d72a341217cf99741a60a996f982f3a
Gabriela-Guerra <gabrielaguerra.arq@gmail.com>
2021-10-25 15:53:37 -0300

    desafio 2, ajuste CC

--- desafio1.sql
+++ desafio1.sql
@@ -1,5 +1,5 @@
 -- 1 - Exiba os países e indicando se cada um deles se encontra ou não na região formada pela Europa
-SELECT COUNTRY_NAME AS País, 
+SELECT COUNTRY_NAME AS 'País', 
 IF(REGION_ID = 1,
 'incluído',
 'não-incluído') AS 'Status Inclusão'
