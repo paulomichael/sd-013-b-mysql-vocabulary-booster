e7560355ed1862df528156c1237e5eb22ecdb555
Demontier <demontier@outlook.com>
2021-10-24 21:14:35 -0300

    requisito 4

---
+++ desafio4.sql
@@ -0,0 +1,15 @@
+SELECT 
+    t2.JOB_TITLE AS 'Cargo',
+    ROUND(AVG(t1.SALARY), 2) AS 'Média salarial',
+    CASE
+        WHEN ROUND(AVG(t1.SALARY), 2) BETWEEN 2000 AND 5800 THEN 'Júnior'
+        WHEN ROUND(AVG(t1.SALARY), 2) BETWEEN 5801 AND 7500 THEN 'Pleno'
+        WHEN ROUND(AVG(t1.SALARY), 2) BETWEEN 7501 AND 10500 THEN 'Sênior'
+        ELSE 'CEO'
+    END AS 'Senioridade'
+FROM
+    hr.employees AS t1
+        INNER JOIN
+    hr.jobs AS t2 ON t1.JOB_ID = t2.JOB_ID
+GROUP BY t2.JOB_TITLE
+ORDER BY ROUND(AVG(t1.SALARY), 2) , t2.JOB_TITLE;
