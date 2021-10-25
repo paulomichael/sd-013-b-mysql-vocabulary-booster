3a3fc31ce449d6d303f49b669bba0475b758ae62
Lucas-L-S-Haine <lucaslshaine@gmail.com>
2021-10-25 19:35:26 -0300

    Desafio07. Adicionada label "Salário".

--- desafio7.sql
+++ desafio7.sql
@@ -1,7 +1,7 @@
 SELECT
 UCASE(CONCAT(E.FIRST_NAME, ' ', E.LAST_NAME)) AS 'Nome completo',
 JH.START_DATE AS 'Data de início do cargo',
-E.SALARY
+E.SALARY AS 'Salário'
 FROM hr.employees AS E
 INNER JOIN hr.jobs AS J
 INNER JOIN hr.job_history AS JH
