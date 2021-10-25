6fe4a6ccf2751bfd99cab475488af96ee2758152
Joao Victor <joaogmavictor@gmail.com>
2021-10-25 19:21:15 -0300

    Arrumando Code Climate

--- desafio8.sql
+++ desafio8.sql
@@ -1,7 +1,7 @@
 SELECT 
-	C.ContactName AS `Nome de contato`,
-	S.ShipperName AS `Empresa que fez o envio`,
-    O.OrderDate AS `Data do pedido`
+C.ContactName AS `Nome de contato`,
+S.ShipperName AS `Empresa que fez o envio`,
+O.OrderDate AS `Data do pedido`
 FROM w3schools.orders AS O
 INNER JOIN w3schools.shippers AS S
 ON S.ShipperID = O.ShipperID
