c66452f84c660817d909ea1a35deb2c92a701dab
Carol_Inglethe <carolinainglethe@gmail.com>
2021-10-25 17:06:08 -0300

    Desafio 4 - correcao

--- desafio4.sql
+++ desafio4.sql
@@ -1,12 +1,12 @@
-SELECT JO.`JOB_TITLE` AS `Cargo`, ROUND(AVG(EM.`SALARY`),2) AS `Média salarial`,
+SELECT JO.`JOB_TITLE` AS `Cargo`, ROUND(EM.`SALARY` / 2 ,2) AS `Média salarial`,
 	CASE
-      WHEN `Média salarial` > 2000 AND `Média salarial` < 5800 THEN 'Júnior'
-	  WHEN `Média salarial` > 5801 AND `Média salarial` < 7500 THEN 'Pleno'
-	  WHEN `Média salarial` > 7501 AND `Média salarial` < 10500 THEN 'Sênior'
-      WHEN `Média salarial` > 10500 THEN 'CEO'
+      WHEN  ROUND(EM.`SALARY` / 2 ,2) > 2000 AND  ROUND(EM.`SALARY` / 2 ,2)< 5800 THEN 'Júnior'
+	  WHEN  ROUND(EM.`SALARY` / 2 ,2) > 5801 AND  ROUND(EM.`SALARY` / 2 ,2) < 7500 THEN 'Pleno'
+	  WHEN  ROUND(EM.`SALARY` / 2 ,2) > 7501 AND  ROUND(EM.`SALARY` / 2 ,2) < 10500 THEN 'Sênior'
+      WHEN  ROUND(EM.`SALARY` / 2 ,2) > 10500 THEN 'CEO'
     ELSE NULL
     END AS `Senioridade`
   FROM hr.employees AS EM
   JOIN hr.jobs AS JO
   ON JO.`JOB_ID` = EM.`JOB_ID`
-ORDER BY `Média salarial`, `Cargo`;
+ORDER BY `Média salarial`, JO.`JOB_TITLE`;
