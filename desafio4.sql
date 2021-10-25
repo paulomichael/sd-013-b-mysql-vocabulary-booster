87d2351aa8afaa49ad29b744302f2ebc4f339a6f
MilenaCLeme <milenaleme4@hotmail.com>
2021-10-25 16:07:08 -0300

    Desafio 4 finalizado

---
+++ desafio4.sql
@@ -0,0 +1,14 @@
+SELECT 
+    e.JOB_ID AS `Cargo`,
+    ROUND(AVG(e.SALARY), 2) AS `Média salarial`,
+    CASE
+        WHEN ROUND(AVG(e.SALARY), 2) BETWEEN 2000 AND 5800 THEN 'Júnior'
+        WHEN ROUND(AVG(e.SALARY), 2) BETWEEN 5801 AND 7500 THEN 'Pleno'
+        WHEN ROUND(AVG(e.SALARY), 2) BETWEEN 7501 AND 10500 THEN 'Sênior'
+        ELSE 'CEO'
+    END AS Senioridade
+FROM
+    hr.employees AS e
+        JOIN
+    hr.jobs AS j ON e.JOB_ID = j.JOB_ID
+GROUP BY e.JOB_ID;
