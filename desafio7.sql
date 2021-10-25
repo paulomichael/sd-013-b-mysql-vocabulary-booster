77bfcf77c2b4b40d71c24c5466e9efc5c63f7200
EddieRAM <edusacarneiro@gmail.com>
2021-10-25 18:57:50 -0300

    req 7 - date filter fix, req 8

--- desafio7.sql
+++ desafio7.sql
@@ -3,5 +3,7 @@ SELECT
     HIRE_DATE AS 'Data de início',
     SALARY AS 'Salário'
 FROM
-    hr.employees e
-ORDER BY CONCAT(FIRST_NAME, ' ',LAST_NAME) DESC , HIRE_DATE;
+    hr.employees
+WHERE
+    MONTH(HIRE_DATE) BETWEEN 1 AND 3
+ORDER BY CONCAT(FIRST_NAME, ' ', LAST_NAME) DESC , HIRE_DATE
