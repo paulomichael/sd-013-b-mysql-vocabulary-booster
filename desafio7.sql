f017d36ed3505f992ed94061afbc7b64a27a9b20
Lucas-L-S-Haine <lucaslshaine@gmail.com>
2021-10-25 19:42:04 -0300

    Desafio07. Agora vai.

--- desafio7.sql
+++ desafio7.sql
@@ -1,6 +1,6 @@
 SELECT
 UCASE(CONCAT(E.FIRST_NAME, ' ', E.LAST_NAME)) AS 'Nome completo',
-JH.START_DATE AS 'Data de início do cargo',
+JH.START_DATE AS 'Data de início',
 E.SALARY AS 'Salário'
 FROM hr.employees AS E
 INNER JOIN hr.jobs AS J
