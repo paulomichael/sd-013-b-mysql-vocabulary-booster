765faa45989f311a1a8d9f63e79f35117bf0ce5a
Felipe Ventorim <felipeventorim11@gmail.com>
2021-10-25 15:42:29 -0300

    Desafio6

--- desafio6.sql
+++ desafio6.sql
@@ -4,7 +4,7 @@ SELECT
     e.HIRE_DATE AS `Data de início do cargo`,
     d.DEPARTMENT_NAME AS `Departamento`
 FROM hr.employees AS e
-		JOIN hr.job_history AS jh ON jh.EMPLOYEE_ID = e.EMPLOYEE_ID
-		JOIN hr.jobs AS j ON e.JOB_ID = j.JOB_ID
-		JOIN hr.departments AS d ON d.DEPARTMENT_ID = e.DEPARTMENT_ID
+	JOIN hr.job_history AS jh ON jh.EMPLOYEE_ID = e.EMPLOYEE_ID
+	JOIN hr.jobs AS j ON e.JOB_ID = j.JOB_ID
+	JOIN hr.departments AS d ON d.DEPARTMENT_ID = e.DEPARTMENT_ID
 ORDER BY `Nome completo` DESC , Cargo;
