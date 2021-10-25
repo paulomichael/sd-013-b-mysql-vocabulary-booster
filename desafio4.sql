8c2b627a86a69b3fb563c5cbc7904301c3ee7fec
Carol_Inglethe <carolinainglethe@gmail.com>
2021-10-25 19:54:52 -0300

    Refatora 4 -volta  usar 2 tabelas, descomenta

--- desafio4.sql
+++ desafio4.sql
@@ -1,10 +1,13 @@
--- SELECT `JOB_ID` AS `Cargo`, ROUND(`SALARY` / 2 ,2) AS `Média salarial`,
--- 	CASE
---       WHEN  ROUND(`SALARY` / 2 ,2) BETWEEN  2000 AND 5800 THEN 'Júnior'
--- 	  WHEN  ROUND(`SALARY` / 2 ,2) BETWEEN  5801 AND 7500 THEN 'Pleno'
--- 	  WHEN  ROUND(`SALARY` / 2 ,2) BETWEEN  7501 AND 10500 THEN 'Sênior'
---       WHEN  ROUND(`SALARY` / 2 ,2) > 10500 THEN 'CEO'
---     ELSE NULL
---     END AS `Senioridade`
--- FROM hr.employees
--- ORDER BY `Média salarial`, `JOB_ID`;
+SELECT JO.`JOB_TITLE` AS `Cargo`, 
+ROUND(EM.`SALARY` / 2 ,2) AS `Média salarial`,
+CASE
+WHEN  ROUND(`SALARY` / 2 ,2) BETWEEN  2000 AND 5800 THEN 'Júnior'
+WHEN  ROUND(`SALARY` / 2 ,2) BETWEEN  5801 AND 7500 THEN 'Pleno'
+WHEN  ROUND(`SALARY` / 2 ,2) BETWEEN  7501 AND 10500 THEN 'Sênior'
+WHEN  ROUND(EM.`SALARY` / 2 ,2) > 10500 THEN 'CEO'
+ELSE NULL
+END AS `Senioridade`
+FROM hr.employees AS EM
+JOIN hr.jobs AS JO
+ON JO.`JOB_ID` = EM.`JOB_ID`
+ORDER BY `Média salarial`, `Cargo`;
