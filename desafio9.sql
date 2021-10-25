4d3c5154c2b37ea0b24dd21e2652625a521e6d7b
Lucas-L-S-Haine <lucaslshaine@gmail.com>
2021-10-25 20:15:59 -0300

    Desafio09.

---
+++ desafio9.sql
@@ -0,0 +1,9 @@
+SELECT
+CONCAT(E.FirstName, ' ', E.LastName) AS 'Nome completo',
+COUNT(O.OrderDate) AS 'Total de pedidos'
+FROM w3schools.employees AS E
+JOIN w3schools.orders AS O
+ON E.EmployeeID = O.EmployeeID
+GROUP BY `Nome completo`
+ORDER BY `Total de pedidos` ASC
+;
