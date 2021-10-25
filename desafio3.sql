38970ed4d3054270f4960f957a720c166f1f328c
Aleilton Chavenco <aleilton@gmail.com>
2021-10-25 18:19:20 -0300

    Requisito 3 - correção

--- desafio3.sql
+++ desafio3.sql
@@ -1,7 +1,6 @@
-SELECT 
+SELECT
     job_title AS `Cargo`,
     max_salary - min_salary AS `Diferença entre salários máximo e mínimo`
 FROM
     hr.jobs
 ORDER BY `Diferença entre salários máximo e mínimo`, `Cargo`;
-    
