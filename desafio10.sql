c7c20f60df67c796773f58f8a187d33caa352d90
José Demeneghi <josedemeneghi_cba@hotmail.com>
2021-10-25 14:20:16 -0400

    requisito 10

---
+++ desafio10.sql
@@ -0,0 +1,10 @@
+SELECT
+P.ProductName AS 'Produto',
+MIN(OD.Quantity) AS 'Mínima',
+MAX(OD.Quantity) AS 'Máxima',
+ROUND(AVG(OD.Quantity), 2) AS 'Média'
+FROM w3schools.order_details OD
+JOIN w3schools.products P ON OD.ProductID = P.ProductID
+GROUP BY OD.ProductID
+HAVING `Média` > 20
+ORDER BY `Média`, `Produto`;
