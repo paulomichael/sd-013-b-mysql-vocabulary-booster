eb68a8b3ea6e1de1c6b996ee98256a10b8cf69af
Julio Pereira <jandrade.pereira1@gmail.com>
2021-10-25 19:00:20 -0300

    Requisito 4 - corrige o requisito

--- desafio4.sql
+++ desafio4.sql
@@ -8,5 +8,5 @@ WHEN AVG(employees_table.SALARY) > 10500 THEN 'CEO'
 END AS `Senioridade`
 FROM hr.employees AS employees_table
 INNER JOIN hr.jobs AS jobs_table ON employees_table.JOB_ID = jobs_table.JOB_ID
-GROUP BY jobs_table.JOB_TITLE
+GROUP BY `Cargo`
 ORDER BY `Média Salarial`, `Cargo`;
