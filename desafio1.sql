2844f34737b106b12b76f92fccccc664c8f40f70
vitor hugo da silva <vitorhugo.dev1997@gmail.com>
2021-10-25 15:53:09 -0300

    desafios 1 e 2- code climate

--- desafio1.sql
+++ desafio1.sql
@@ -2,5 +2,4 @@ SELECT c.COUNTRY_NAME AS País, IF(c.REGION_ID = 1, 'incluído', 'não incluído
 FROM hr.countries AS c
 INNER JOIN hr.regions AS r
 ON c.REGION_ID = r.REGION_ID
-ORDER BY c.COUNTRY_NAME
-;
+ORDER BY c.COUNTRY_NAME;
