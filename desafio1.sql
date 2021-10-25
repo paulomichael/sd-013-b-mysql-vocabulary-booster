ae0aeea472acdf9e25412de4d4625b1277ad0836
JoãoBDf <joaobdf@hotmail.com>
2021-10-25 15:09:03 -0300

    O novo lint D-1

--- desafio1.sql
+++ desafio1.sql
@@ -1,4 +1,9 @@
-SELECT country_name AS `País`,
-		IF (region_id = 1, 'incluído', 'não incluído') AS `Status Inclusão`
-        FROM hr.countries
-        ORDER BY country_name;
+SELECT 
+    country_name AS País,
+    IF(region_id = 1,
+        'incluído',
+        'não incluído') AS `Status Inclusão`
+FROM
+    hr.countries
+ORDER BY country_name;
+        
