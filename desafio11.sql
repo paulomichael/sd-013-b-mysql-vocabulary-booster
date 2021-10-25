185e30df7f1d5dc0a57c7e9513faf3aaf3286a48
Marcos Estevao <dusdoom@gmail.com>
2021-10-25 15:45:18 -0300

    fix challenge 11

--- desafio11.sql
+++ desafio11.sql
@@ -1,13 +1,16 @@
 SELECT
 a.contactname AS `Nome`,
-b.country AS `País`,
-COUNT(*) - 1 AS `Número de compatriotas`
+ANY_VALUE(a.country) AS `País`,
+-- workaround found in https://stackoverflow.com/questions/34115174/error-related-to-only-full-group-by-when-executing-a-query-in-mysql
+COUNT(*) AS `Número de compatriotas`
 FROM
 w3schools.customers AS a,
 w3schools.customers AS b
 WHERE
 a.country = b.country
-GROUP BY `Nome`
+AND a.customerid <> b.customerid
+GROUP BY
+	`Nome`
 ORDER BY
 `Nome` ASC,
 `País` ASC;
