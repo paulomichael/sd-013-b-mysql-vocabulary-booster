90c6a714dc35c9919f42fa35065f4899bd1c2ba6
matheuscarvalhoscm <matheuscarvalhoscm@gmail.com>
2021-10-25 19:56:38 -0300

    Requisito 16

---
+++ desafio16.sql
@@ -0,0 +1,21 @@
+USE hr;
+DELIMITER $$
+
+CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario(email VARCHAR(300))
+RETURNS INT READS SQL DATA
+BEGIN 
+  DECLARE totalDeCargos INT;
+  SELECT
+    COUNT(JH.JOB_ID)
+  FROM
+    job_history AS JH
+    INNER JOIN employees AS E ON E.EMPLOYEE_ID = JH.EMPLOYEE_ID AND E.EMAIL = email
+  INTO totalDeCargos;
+  RETURN totalDeCargos;
+END $$
+
+DELIMITER ;
+
+SELECT buscar_quantidade_de_empregos_por_funcionario('NKOCHHAR');
+    
+  
