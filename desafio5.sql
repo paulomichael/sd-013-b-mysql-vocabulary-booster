99d30bf3338cd3ebedd55c4acf2afbe9997d4248
Joao Victor Cristino F <jvictorfranca@yahoo.com.br>
2021-10-25 19:22:02 -0300

    resolve as medias

--- desafio5.sql
+++ desafio5.sql
@@ -1,8 +1,8 @@
 SELECT t2.JOB_TITLE AS Cargo, 
 t2.MAX_SALARY - t2.MIN_SALARY
 AS "Variação Salarial",
-t2.MAX_SALARY AS  "Média mínima mensal" ,
-t2.MIN_SALARY AS "Média máxima mensal"
+ROUND(t2.MAX_SALARY/12,2) AS  "Média máxima mensal" ,
+ROUND(t2.MIN_SALARY/12,2) AS "Média mínima mensal"
 FROM employees AS t1
 INNER JOIN jobs AS t2
 ON t1.JOB_ID = t2.JOB_ID
