3d16499ac270ec0b65740591a55d16514598a27c
giiffonseca <guii.ffonseca22@gmail.com>
2021-10-25 15:54:59 -0300

    Testando Requisto 01

---
+++ desafio1.sql
@@ -0,0 +1,6 @@
+SELECT t1.COUNTRY_NAME AS `País`, 
+IF(t2.REGION_ID = 1, 'incluído', 'não incluído') AS  `Status Inclusão` 
+FROM hr.countries AS t1
+INNER JOIN hr.regions AS t2
+ON t1.REGION_ID =  t2.REGION_ID
+ORDER BY COUNTRY_NAME;
