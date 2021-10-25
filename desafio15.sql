3c38b410581aeca12800c54ac86c0a8d89756748
btriz <00btrz@gmail.com>
2021-10-25 13:57:36 -0300

    DESAFIO 15

---
+++ desafio15.sql
@@ -0,0 +1,21 @@
+DROP PROCEDURE IF EXISTS buscar_media_por_cargo;
+
+DELIMITER $$
+CREATE PROCEDURE buscar_media_por_cargo(IN Cargo VARCHAR(50))
+BEGIN
+  SELECT
+    ROUND(AVG(E.SALARY), 2) AS `Média salarial`
+  FROM
+    hr.employees AS E
+  WHERE E.JOB_ID = (
+    SELECT
+      JOB_ID
+    FROM
+      hr.jobs AS J
+    WHERE J.JOB_TITLE = Cargo
+  )
+  GROUP BY JOB_ID;
+END $$
+DELIMITER ;
+
+CALL buscar_media_por_cargo('Programmer');
