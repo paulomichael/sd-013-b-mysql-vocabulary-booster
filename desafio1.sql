d636cdcffd6e7eaf8ab0172e32fc72421a206cd4
Arthur-Jr <arthurjr1906@gmail.com>
2021-10-25 19:37:39 -0300

    Resolve os requisitos 1, 2 e 3.

---
+++ desafio1.sql
@@ -0,0 +1,6 @@
+SELECT C.COUNTRY_NAME AS `País`,
+IF(R.REGION_NAME = 'Europe', 'incluído', 'não incluído') AS `Status Inclusão`
+FROM hr.countries AS C
+JOIN hr.regions AS R
+ON C.REGION_ID = R.REGION_ID
+ORDER BY C.COUNTRY_NAME ASC;
