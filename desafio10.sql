23b658ce115692414eb689ecb35f679db7d3ba17
Alex <alexcorreas1994@gmail.com>
2021-10-25 16:53:14 -0300

    Requisito 10 50% Requisitos totais

---
+++ desafio10.sql
@@ -0,0 +1,10 @@
+SELECT ProductName AS `Produto`,
+MIN(od.quantity) AS `Mínima`,
+MAX(od.quantity) AS `Máxima`,
+ROUND(AVG(quantity), 2) AS `Média`
+FROM w3schools.products AS p
+INNER JOIN w3schools.order_details AS od ON p.ProductID = od.ProductID
+GROUP BY `Produto`
+HAVING `Média` > 20.00
+ORDER BY `Média`, `Produto`;
+ 
