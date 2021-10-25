60151a5de4fd3fa93bf126f2048bf6b9c277c66e
GuilhermeLaraRusso <guilhermelrusso@gmail.com>
2021-10-25 16:50:14 -0300

    ajustado code climate 2.2

--- desafio15.sql
+++ desafio15.sql
@@ -3,11 +3,11 @@ DELIMITER $$
 
 CREATE PROCEDURE buscar_media_por_cargo(IN cargo VARCHAR(100))
 BEGIN
-	SELECT ROUND(AVG(e.SALARY), 2) AS 'Média salarial'
-    FROM hr.employees AS e
-    INNER JOIN hr.jobs AS j
-    ON j.JOB_ID = e.JOB_ID
-    WHERE j.JOB_TITLE = cargo;
+SELECT ROUND(AVG(e.SALARY), 2) AS 'Média salarial'
+FROM hr.employees AS e
+INNER JOIN hr.jobs AS j
+ON j.JOB_ID = e.JOB_ID
+WHERE j.JOB_TITLE = cargo;
 END $$
 
 DELIMITER ;
--- desafio16.sql
+++ desafio16.sql
@@ -4,13 +4,13 @@ DELIMITER $$
 CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario(email VARCHAR(50))
 RETURNS INT READS SQL DATA
 BEGIN
-	DECLARE emprego_total INT;
-    SELECT COUNT(*)
-    FROM hr.job_history AS j
-    INNER JOIN hr.employees AS e
-    ON j.EMPLOYEE_ID = e.EMPLOYEE_ID
-    WHERE e.EMAIL = email INTO emprego_total;
-    RETURN emprego_total;
+DECLARE emprego_total INT;
+SELECT COUNT(*)
+FROM hr.job_history AS j
+INNER JOIN hr.employees AS e
+ON j.EMPLOYEE_ID = e.EMPLOYEE_ID
+WHERE e.EMAIL = email INTO emprego_total;
+RETURN emprego_total;
 END $$
 
 DELIMITER ;
