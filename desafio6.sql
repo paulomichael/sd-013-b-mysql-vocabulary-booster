caf198ea429871143df44f464939b091571227a5
Felipe Ventorim <felipeventorim11@gmail.com>
2021-10-25 16:13:50 -0300

    Desafio6

--- desafio6.sql
+++ desafio6.sql
@@ -5,6 +5,6 @@ SELECT
     d.DEPARTMENT_NAME AS `Departamento`
 FROM hr.employees AS e
 JOIN hr.job_history AS jh ON jh.EMPLOYEE_ID = e.EMPLOYEE_ID
-JOIN hr.jobs AS j ON e.JOB_ID = j.JOB_ID
+JOIN hr.jobs AS j ON jh.JOB_ID = j.JOB_ID
 JOIN hr.departments AS d ON d.DEPARTMENT_ID = e.DEPARTMENT_ID
 ORDER BY `Nome completo` DESC , Cargo;
