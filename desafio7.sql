fc277258a33c7fbff6876cea53a2daa7e93fa422
luis <luizcruz0809@gmail.com>
2021-10-25 17:31:31 -0300

    test

--- desafio7.sql
+++ desafio7.sql
@@ -1,7 +1,7 @@
 SELECT
-		UCASE(CONCAT(E.first_name, ' ', E.last_name)) `Nome completo`,
-    JH.START_DATE `Data de início`,
-    E.SALARY `Salário`
+UCASE(CONCAT(E.first_name, ' ', E.last_name)) `Nome completo`,
+JH.START_DATE `Data de início`,
+E.SALARY `Salário`
 FROM employees E
 INNER JOIN job_history JH
 ON JH.EMPLOYEE_ID = E.EMPLOYEE_ID
