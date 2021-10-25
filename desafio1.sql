ec645f9783a61b213116f286cda41cafdbd7f4cb
Janaina P de Oliveira Minutti <janainapp@gmail.com>
2021-10-25 18:18:17 -0300

    correção codeclimate

--- desafio1.sql
+++ desafio1.sql
@@ -1,7 +1,8 @@
-SELECT COUNTRY_NAME AS 'País', 
+SELECT 
+COUNTRY_NAME AS 'País', 
 CASE 
-WHEN REGION_ID = 1 THEN 'incluído'
-ELSE 'não incluído'
+  WHEN REGION_ID = 1 THEN 'incluído'
+  ELSE 'não incluído'
 END AS 'Status Inclusão'
 FROM hr.countries
 ORDER BY País;
