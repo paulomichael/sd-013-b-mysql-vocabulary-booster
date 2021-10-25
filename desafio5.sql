fd1af0d37cb69fe889798576e2e60e6a57efe28f
Julio Pereira <jandrade.pereira1@gmail.com>
2021-10-25 18:18:39 -0300

    Requisito 5 - inclui a tabela jobs no lugar da employees

--- desafio5.sql
+++ desafio5.sql
@@ -2,5 +2,5 @@ SELECT JOB_TITLE AS `Cargo`,
     MAX_SALARY - MIN_SALARY AS `Variação Salarial`,
 	  ROUND(MIN_SALARY/12, 2) AS `Média mínima mensal`,
     ROUND(MAX_SALARY/12, 2) AS `Média máxima mensal`
-FROM hr.employees
+FROM hr.jobs
 ORDER BY `Variação Salarial` ASC, `Cargo`;
