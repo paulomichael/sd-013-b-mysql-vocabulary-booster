3dab265dcc7c1b05d05f544e7aca2597eb4b01d1
Alex <alexcorreas1994@gmail.com>
2021-10-25 14:37:45 -0300

    Requisito 1 feito

--- desafio1.sql
+++ desafio1.sql
@@ -1,5 +1,4 @@
-SELECT COUNTRY_NAME AS País
+SELECT COUNTRY_NAME AS País,
 IF(REGION_ID = 1, 'incluído', 'não incluído') AS `Status Inclusão`
-FROM hr.countries 
+FROM hr.countries
 ORDER BY País;
-
