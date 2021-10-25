dcdd8b69ff88c8fdee2d12429192c3850a9f1f48
Alex <alexcorreas1994@gmail.com>
2021-10-25 18:29:41 -0300

    Resolvendo erros de codeclimate de novo

--- desafio11.sql
+++ desafio11.sql
@@ -1,9 +1,9 @@
 SELECT cust01.ContactName AS `Nome`,
 cust01.Country AS `País`,
- COUNT(cust01.country) AS `Número de compatriotas`
- FROM w3schools.customers AS cust01, w3schools.customers AS cust02
- WHERE cust01.Country = cust02.Country
- AND cust01.CustomerID <> cust02.CustomerID
- GROUP BY cust01.CustomerID
- ORDER BY cust01.ContactName;
+COUNT(cust01.country) AS `Número de compatriotas`
+FROM w3schools.customers AS cust01, w3schools.customers AS cust02
+WHERE cust01.Country = cust02.Country
+AND cust01.CustomerID <> cust02.CustomerID
+GROUP BY cust01.CustomerID
+ORDER BY cust01.ContactName;
  
