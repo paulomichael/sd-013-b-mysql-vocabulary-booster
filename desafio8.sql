18a6faa7fade48cf7574ada1b38e6b21b2639fef
Alef Sloan <Alef_sloan@hotmail.com>
2021-10-25 19:25:47 -0300

    Corrigindo erros 2 - Requisito 8

--- desafio8.sql
+++ desafio8.sql
@@ -6,4 +6,4 @@ INNER JOIN w3schools.orders AS orders ON orders.OrderID = order_details.OrderID
 INNER JOIN w3schools.shippers AS shippers ON shippers.ShipperID = orders.ShipperID
 INNER JOIN w3schools.customers AS customers ON customers.CustomerID = orders.CustomerID
 WHERE ShipperName = 'United Package' OR ShipperName = 'Speedy Express'
-ORDER BY `Nome de contato` ASC, `Empresa que fez o envio` ASC, `Data do envio` ASC;
+ORDER BY `Nome de contato` ASC, `Empresa que fez o envio` ASC, `Data do pedido` ASC;
