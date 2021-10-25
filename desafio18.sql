99776d5049a8c67d3e56cef6d7956023621ef0bb
José Demeneghi <josedemeneghi_cba@hotmail.com>
2021-10-25 16:03:48 -0400

    requisito 18

---
+++ desafio18.sql
@@ -0,0 +1,8 @@
+SELECT
+CONCAT(E.FIRST_NAME, ' ', E.LAST_NAME) AS 'Nome completo',
+CONCAT(SUBSTRING(JH.START_DATE,9,2),'/',SUBSTRING(JH.START_DATE,6,2),'/',YEAR(JH.START_DATE)) AS 'Data de início',
+CONCAT(SUBSTRING(JH.END_DATE,9,2),'/',SUBSTRING(JH.END_DATE,6,2),'/',YEAR(JH.END_DATE)) AS 'Data de rescisão',
+ROUND(DATEDIFF(JH.END_DATE, JH.START_DATE)/365, 2) AS 'Anos trabalhados'
+FROM hr.job_history JH
+JOIN hr.employees E ON JH.EMPLOYEE_ID = E.EMPLOYEE_ID
+ORDER BY `Nome completo`, `Anos trabalhados`;
