db3fae180bef10b7900b1c65b26427c85d93de65
Guilherme da Costa <guilhermedc93@gmail.com>
2021-10-25 14:47:47 -0300

    conclui desafio 20 finalizado 17/10/21

---
+++ desafio20.sql
@@ -0,0 +1,20 @@
+USE hr;
+DELIMITER $$
+CREATE PROCEDURE exibir_historico_completo_por_funcionario(IN employeeEmail VARCHAR(50))
+BEGIN
+SELECT 
+    CONCAT(E.FIRST_NAME, ' ', E.LAST_NAME) AS `Nome completo`,
+     D.DEPARTMENT_NAME AS Departamento,
+     J.JOB_TITLE AS Cargo
+FROM
+    hr.job_history as JH
+    JOIN hr.employees AS E
+    ON JH.EMPLOYEE_ID = E.EMPLOYEE_ID
+    JOIN hr.departments AS D
+    ON JH.DEPARTMENT_ID = D.DEPARTMENT_ID
+    JOIN hr.jobs AS J
+    ON J.JOB_ID = JH.JOB_ID
+    WHERE EMAIL = employeeEmail
+    ORDER BY Departamento, Cargo;
+END $$
+DELIMITER ;
