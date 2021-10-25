c2685f83d7ba04bb951a36b6cb45bca9f48d3630
Gabriela-Guerra <gabrielaguerra.arq@gmail.com>
2021-10-25 15:48:43 -0300

    desafio 2

--- desafio1.sql
+++ desafio1.sql
@@ -1,7 +1,7 @@
 -- 1 - Exiba os países e indicando se cada um deles se encontra ou não na região formada pela Europa
-SELECT country_name AS País, 
+SELECT COUNTRY_NAME AS País, 
 IF(REGION_ID = 1,
 'incluído',
 'não-incluído') AS 'Status Inclusão'
 FROM hr.countries
-ORDER BY País;
+ORDER BY País ASC;
