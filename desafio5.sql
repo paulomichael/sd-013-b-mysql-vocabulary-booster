978342cd2e46528a3b9220e71867cc5e9c1fe0ff
Lopes <gus.lcampos38@gmail.com>
2021-10-25 16:16:10 -0300

    desafio 5

---
+++ desafio5.sql
@@ -0,0 +1,8 @@
+select
+	job_title as `Cargo`,
+    round(max_salary - min_salary) as `Variação Salarial`,
+    round(min_salary / 12, 2) as `Média mínima mensal`,
+    round(max_salary / 12, 2) as `Média máxima mensal`
+from
+	hr.jobs
+order by `Variação Salarial`, `Cargo`;
