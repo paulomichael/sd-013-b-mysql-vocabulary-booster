c332ea99ad11b7a841785e1a5e1728031ab32bc7
Carol_Inglethe <carolinainglethe@gmail.com>
2021-10-25 20:38:52 -0300

    Refatora Desafio 8

--- desafio8.sql
+++ desafio8.sql
@@ -1,7 +1,10 @@
-SELECT cust.`ContactName` AS `Nome de contato`,
+SELECT 
+cust.`ContactName` AS `Nome de contato`,
 ship.`ShipperName` AS `Empresa que fez o envio`, 
 o.`OrderDate` AS `Data do pedido`
 FROM w3schools.customers AS cust
 JOIN w3schools.shippers AS ship
 JOIN w3schools.orders AS o
-ON cust.`CustomerID` = o.`CustomerID` AND ship.`ShipperID` = o.`ShipperID`;
+ON cust.`CustomerID` = o.`CustomerID` AND ship.`ShipperID` = o.`ShipperID`
+WHERE ship.`ShipperName` IN ('Speedy Express', 'United Package')
+ORDER BY `Nome de contato`, `Empresa que fez o envio`, `Data do pedido`;
