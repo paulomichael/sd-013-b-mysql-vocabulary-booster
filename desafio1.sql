b1d23f0c64696744ca09b409d28447acacba007b
GenesisHenriques <genesishenriques7@gmail.com>
2021-10-25 15:11:02 -0300

    .

--- desafio1.sql
+++ desafio1.sql
@@ -1,14 +1,8 @@
 SELECT 
-    C.COUNTRY_NAME AS 'País',
-    IF(R.REGION_ID = 1,
+    COUNTRY_NAME AS País,
+    IF(REGION_ID = 1,
         'incluído',
         'não incluído') AS 'Status Inclusão'
 FROM
-    hr.countries AS C
-        INNER JOIN
-    hr.regions AS R
-WHERE
-    C.REGION_ID = R.REGION_ID
-    ORDER BY País;
-
-  
+    hr.countries
+ORDER BY COUNTRY_NAME;
