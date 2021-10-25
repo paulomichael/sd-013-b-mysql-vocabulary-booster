995747cd2d90724026128d6eb83c3372581551e5
MilenaCLeme <milenaleme4@hotmail.com>
2021-10-25 20:10:29 -0300

    Desafio 6 arrumando

--- desafio6.sql
+++ desafio6.sql
@@ -1,13 +1,14 @@
 SELECT 
     CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) AS 'Nome completo',
     j.JOB_TITLE AS 'Cargo',
-    e.HIRE_DATE AS 'Data de início do cargo',
+    jh.START_DATE AS 'Data de início do cargo',
     d.DEPARTMENT_NAME AS 'Departamento'
 FROM
     hr.employees AS e
         JOIN
-    hr.jobs AS j
+    hr.jobs AS j ON j.JOB_ID = e.JOB_ID
         JOIN
-    hr.departments AS d ON e.JOB_ID = j.JOB_ID
-        AND e.DEPARTMENT_ID = d.DEPARTMENT_ID
+    hr.job_history AS jh ON jh.EMPLOYEE_ID = e.EMPLOYEE_ID
+        JOIN
+    hr.departments AS d ON d.DEPARTMENT_ID = jh.DEPARTMENT_ID
 ORDER BY `Nome completo` DESC , `Cargo`;
