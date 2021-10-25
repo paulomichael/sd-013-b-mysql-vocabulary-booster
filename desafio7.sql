120deb69729dd629210ed92799aa084e4e79f9bb
GenesisHenriques <genesishenriques7@gmail.com>
2021-10-25 19:05:37 -0300

    Desafio 7 pronto

---
+++ desafio7.sql
@@ -0,0 +1,10 @@
+SELECT 
+    UCASE(CONCAT(FIRST_NAME, ' ', LAST_NAME)) AS `Nome completo`,
+    j_h.START_DATE AS `Data de início`,
+    SALARY AS `Salário`
+FROM
+    hr.employees AS e
+    INNER JOIN hr.job_history AS j_h ON e.EMPLOYEE_ID = j_h.EMPLOYEE_ID
+    WHERE MONTH(j_h.START_DATE) IN (01, 02, 03)
+    ORDER BY `Nome completo` ASC;
+    
