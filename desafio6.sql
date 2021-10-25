619b150e9d20e309dd40c1e32b8e7b7eab6acb51
gustavoalves23 <gustavo.alves388@gmail.com>
2021-10-25 15:12:08 -0300

    desafio 6 correcao

--- desafio6.sql
+++ desafio6.sql
@@ -5,11 +5,10 @@ SELECT
     D.department_name AS 'Departamento'
 FROM
     hr.jobs AS J
-		INNER JOIN
-	hr.job_history AS JH ON JH.job_id = j.job_id
         INNER JOIN
-	hr.employees AS E ON E.employee_id = JH.employee_id
+    hr.job_history AS JH ON JH.job_id = j.job_id
+        INNER JOIN
+    hr.employees AS E ON E.employee_id = JH.employee_id
         INNER JOIN
     hr.departments AS D ON D.department_id = JH.department_id
-
 ORDER BY CONCAT(E.first_name, ' ', E.last_name) DESC , J.JOB_TITLE;
