ae2c3293ff42ef2caa1e559e5b6e1daff7cd5652
Rafael Nery Machado <rafaelnerymachado@gmail.com>
2021-10-25 17:02:56 -0300

    requisito 6 - concluido

---
+++ desafio6.sql
@@ -0,0 +1,18 @@
+select * from hr.employees;
+
+select * from hr.job_history;
+
+SELECT 
+    CONCAT(emp.FIRST_NAME, ' ', emp.LAST_NAME) AS `Nome completo`,
+    jobs.JOB_TITLE AS Cargo,
+    jh.START_DATE AS `Data de início do cargo`,
+    dep.DEPARTMENT_NAME AS Departamento
+FROM
+    hr.job_history AS jh
+        INNER JOIN
+    hr.employees AS emp ON jh.EMPLOYEE_ID = emp.EMPLOYEE_ID
+        INNER JOIN
+    hr.jobs AS jobs ON jh.JOB_ID = jobs.JOB_ID
+        INNER JOIN
+    hr.departments AS dep ON jh.DEPARTMENT_ID = dep.DEPARTMENT_ID
+ORDER BY Nome completo DESC , Cargo;
