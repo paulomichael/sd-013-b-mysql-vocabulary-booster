cda9ababf3aee0735d723726e70044f91e581380
Lopes <gus.lcampos38@gmail.com>
2021-10-25 20:16:32 -0300

    desafio2 fixed

--- desafio2.sql
+++ desafio2.sql
@@ -1,12 +1,10 @@
-SELECT * FROM hr.jobs;
-
 SELECT 
     job_title AS `Cargo`,
     CASE
         WHEN max_salary BETWEEN 5000 AND 10000 THEN 'Baixo'
         WHEN max_salary BETWEEN 10001 AND 20000 THEN 'Médio'
         WHEN max_salary BETWEEN 20001 AND 30000 THEN 'Alto'
-        ELSE 'Altíssimo'
+        WHEN max_salary > 30000 THEN 'Altíssimo'
     END AS Nível
 FROM
     hr.jobs
