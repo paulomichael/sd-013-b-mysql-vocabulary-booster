dc3cb0bbc5a488bf3a14e88b89720954b217707a
Joao Victor <joaogmavictor@gmail.com>
2021-10-25 20:55:20 -0300

    Concluindo requisito 17

---
+++ desafio17.sql
@@ -0,0 +1,10 @@
+USE w3schools
+DELIMITER $$
+
+CREATE TRIGGER insertOrderDataInOrders
+	BEFORE INSERT ON w3schools.orders
+    FOR EACH ROW
+BEGIN
+	SET NEW.OrderDate = CURRENT_DATE();
+END $$
+DELIMITER ;
