ab1153e16649975028fa7bc90002b87d4940c13d
JoãoBDf <joaobdf@hotmail.com>
2021-10-25 17:27:22 -0300

    o lint do SQL...

--- desafio7.sql
+++ desafio7.sql
@@ -6,5 +6,4 @@ FROM hr.employees AS e
 JOIN hr.job_history AS jh ON e.employee_id = jh.employee_id
 WHERE MONTH(jh.start_date) IN(01, 02, 03)
 #https://stackoverflow.com/questions/851236/where-clause-to-find-all-records-in-a-specific-month
-ORDER BY `Nome completo`,
-		`Data de início`;
+ORDER BY `Nome completo`, `Data de início`;
