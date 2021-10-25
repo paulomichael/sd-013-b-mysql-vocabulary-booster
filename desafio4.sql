1f1d1e8239776a79fa19a4d70778ecb07a83b14e
Lucas-L-S-Haine <lucaslshaine@gmail.com>
2021-10-25 18:06:19 -0300

    Desafio04. Cargos agrupados por senioridade.

--- desafio4.sql
+++ desafio4.sql
@@ -1,5 +1,12 @@
 SELECT J.JOB_TITLE AS 'Cargo',
-ROUND(AVG(E.SALARY), 2) AS 'Média salarial'
+ROUND(AVG(E.SALARY), 2) AS 'Média salarial',
+    CASE
+        WHEN AVG(E.SALARY) >= 2000 AND AVG(E.SALARY) <= 5800 THEN 'Júnior'
+        WHEN AVG(E.SALARY) > 5800 AND AVG(E.SALARY) <= 7500 THEN 'Pleno'
+        WHEN AVG(E.SALARY) > 7500 AND AVG(E.SALARY) <= 10500 THEN 'Sênior'
+        WHEN AVG(E.SALARY) > 10500 THEN 'CEO'
+    ELSE 'Não Classificado'
+END AS 'Nível'
 FROM hr.jobs AS J
 INNER JOIN hr.employees AS E
 ON J.JOB_ID = E.JOB_ID
