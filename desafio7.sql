3166f7f3ad0217667c04c091292d830df60e05a5
Camila Dornas <camiladornas@gmail.com>
2021-10-25 20:01:28 -0300

    Pelo amor de deus

--- desafio7.sql
+++ desafio7.sql
@@ -3,9 +3,9 @@ SELECT
     jh.START_DATE AS 'Data de início',
     e.SALARY AS 'Salário'
 FROM
-    hr.employees AS e
+    hr.job_history AS jh
         INNER JOIN
-    hr.job_history AS jh ON jh.EMPLOYEE_ID = e.EMPLOYEE_ID
+    hr.employees AS e ON jh.EMPLOYEE_ID = e.EMPLOYEE_ID
 WHERE
     MONTH(jh.START_DATE) IN (01 , 02, 03)
 ORDER BY `Nome completo` , `Data de início`;
