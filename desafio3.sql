3cb51327a5cb98d5bcfb36cdbe53f2c2d8556dfa
Amós Rodrigues <amos.adm.rh@gmail.com>
2021-10-25 18:12:42 -0300

    realiza requisitos 3 e 4

--- desafio3.sql
+++ desafio3.sql
@@ -1,6 +1,6 @@
 SELECT 
     JOB_TITLE AS 'Cargo',
-    (MAX_SALARY - MIN_SALARY) AS 'Diferença entre salários máximo e mínimo'
+    MAX_SALARY - MIN_SALARY AS 'Diferença entre salários máximo e mínimo'
 FROM
     hr.jobs
-ORDER BY (MAX_SALARY - MIN_SALARY) ASC , JOB_TITLE;
+ORDER BY MAX_SALARY - MIN_SALARY ASC , JOB_TITLE;
