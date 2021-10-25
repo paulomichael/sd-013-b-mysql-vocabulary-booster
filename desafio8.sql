13493d526b5be91887eb106c72d205fcad9d4ede
Alef Sloan <Alef_sloan@hotmail.com>
2021-10-25 19:18:16 -0300

    Requisito 8 finalizado

---
+++ desafio8.sql
@@ -0,0 +1,9 @@
+SELECT ContactName AS `Nome de contato`,
+ShipperName AS `Empresa que fez o envio`,
+OrderDate AS `Data do envio`
+FROM w3schools.order_details AS order_details
+INNER JOIN w3schools.orders AS orders ON orders.OrderID = order_details.OrderID
+INNER JOIN w3schools.shippers AS shippers ON shippers.ShipperID = orders.ShipperID
+INNER JOIN w3schools.customers AS customers ON customers.CustomerID = orders.CustomerID
+WHERE ShipperName = 'United Package' OR ShipperName = 'Speedy Express'
+ORDER BY `Nome de contato` ASC, `Empresa que fez o envio` ASC, `Data do envio` ASC;
