4df9384d9fde4d0eebaccc0dc1f1abfcd1355068
Julio Pereira <jandrade.pereira1@gmail.com>
2021-10-25 18:07:13 -0300

    Requisito 5 - corrige exercicio e code climate

--- desafio5.sql
+++ desafio5.sql
@@ -1,8 +1,8 @@
 SELECT 
     jobs_table.JOB_TITLE AS `Cargo`,
     jobs_table.MAX_SALARY - jobs_table.MIN_SALARY AS `Variação Salarial`,
-	ROUND(AVG(jobs_table.MIN_SALARY), 2) AS `Média mínima mensal`,
-    ROUND(AVG(jobs_table.MAX_SALARY), 2) AS `Média máxima mensal`
+	  ROUND(AVG(jobs_table.MIN_SALARY/12), 2) AS `Média mínima mensal`,
+    ROUND(AVG(jobs_table.MAX_SALARY/12), 2) AS `Média máxima mensal`
 FROM
     hr.employees AS employees_table
         INNER JOIN
