65d56d91975563d174432a517a39344566ae7a6e
EddieRAM <edusacarneiro@gmail.com>
2021-10-25 17:18:38 -0300

    req 6

---
+++ desafio6.sql
@@ -0,0 +1,12 @@
+SELECT 
+    CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) AS 'Nome completo',
+    j.JOB_TITLE AS Cargo,
+    e.HIRE_DATE AS 'Data de início do cargo',
+    d.DEPARTMENT_NAME AS 'Departamento'
+FROM
+    hr.employees e
+        INNER JOIN
+    hr.jobs j ON j.JOB_ID = e.JOB_ID
+        INNER JOIN
+    hr.departments d ON d.DEPARTMENT_ID = e.DEPARTMENT_ID
+ORDER BY e.FIRST_NAME DESC , j.JOB_TITLE
