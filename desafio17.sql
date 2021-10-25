a2ccb7903ebf4bae8ba6e2baf2fdee34268eecd6
Felipe Ventorim <felipeventorim11@gmail.com>
2021-10-25 20:05:29 -0300

    Desafio17

--- desafio17.sql
+++ desafio17.sql
@@ -4,7 +4,7 @@ CREATE TRIGGER insere_data_atual_na_orderDate_a_cada_order_criada
 BEFORE INSERT ON orders
 FOR EACH ROW
 BEGIN
-	SET NEW.OrderDate = NOW();
+SET NEW.OrderDate = NOW();
 END $$
 
 DELIMITER ;
