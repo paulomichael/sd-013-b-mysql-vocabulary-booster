ad13eca0497c27f3c20f2f5969d07d37c7a69821
EddieRAM <edusacarneiro@gmail.com>
2021-10-25 15:44:15 -0300

    req 3 fixed order by

--- desafio3.sql
+++ desafio3.sql
@@ -1,3 +1,3 @@
 SELECT JOB_TITLE AS Cargo, (MAX_SALARY - MIN_SALARY) AS 'Diferença entre salários máximo e minimo' 
 FROM hr.jobs 
-ORDER BY 'Diferença entre salários máximo e minimo', Cargo;
+ORDER BY (MAX_SALARY - MIN_SALARY), Cargo;
