f0cb2a1d1b7996be651b249144e1edf76ec88fdb
noel-srocha <manoelrocha.neto.93@hotmail.com>
2021-10-25 15:45:52 -0300

    Challenges 1 to 5 completed

---
+++ desafio3.sql
@@ -0,0 +1,8 @@
+use hr;
+
+SELECT 
+    JOB_TITLE AS 'Cargo',
+    MAX_SALARY - MIN_SALARY AS 'Diferença entre salários máximo e mínimo'
+FROM
+    jobs
+ORDER BY MAX_SALARY - MIN_SALARY , JOB_TITLE;
