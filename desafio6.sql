daa627b9a4c06e3900fda32e3fd6ba89d4366ef0
Lucas Caribé <lucasmoc47@gmail.com>
2021-10-25 19:29:57 -0300

    Requisitos 8, 9 e 10 + correçoes no estilo

--- desafio6.sql
+++ desafio6.sql
@@ -1,8 +1,8 @@
 SELECT
-    CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) AS `Nome completo`,
-    j.JOB_TITLE AS `Cargo`,
-    jh.START_DATE AS `Data de início do cargo`,
-    d.DEPARTMENT_NAME AS `Departamento`
+  CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) AS `Nome completo`,
+  j.JOB_TITLE AS `Cargo`,
+  jh.START_DATE AS `Data de início do cargo`,
+  d.DEPARTMENT_NAME AS `Departamento`
 FROM hr.job_history AS jh
 JOIN hr.employees AS e ON e.EMPLOYEE_ID = jh.EMPLOYEE_ID
 JOIN hr.jobs AS j ON jh.JOB_ID = j.JOB_ID
