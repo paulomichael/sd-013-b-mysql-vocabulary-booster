9f217ba11af3a977053e746bafeb41693cff331f
Lailson Gabriel <lailsongabrielturbo@gmail.com>
2021-10-25 17:26:29 -0300

    identação corrigida

--- desafio7.sql
+++ desafio7.sql
@@ -1,12 +1,12 @@
-SELECT 
-	UCASE(CONCAT(E.first_name, ' ', E.last_name)) AS 'Nome completo',
+SELECT UCASE(CONCAT(E.first_name, ' ', E.last_name)) AS 'Nome completo',
     JH.START_DATE AS 'Data de início',
     E.SALARY AS 'Salário'
 FROM hr.employees AS E
-	INNER JOIN
+INNER JOIN
     hr.job_history AS JH ON E.EMPLOYEE_ID = JH.EMPLOYEE_ID
 WHERE 
-	MONTH(JH.START_DATE) = '01'
-		OR MONTH(JH.START_DATE) = '02'
-        OR MONTH(JH.START_DATE) = '03'
-ORDER BY `Nome Completo` ASC, `Data de início` ASC;
+MONTH(JH.START_DATE) = '01'
+OR MONTH(JH.START_DATE) = '02'
+OR MONTH(JH.START_DATE) = '03'
+ORDER BY `Nome Completo` ASC, `Data de início` ASC
+;
