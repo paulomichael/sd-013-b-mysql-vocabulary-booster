879732dcf09418d261ad0b8d4fd1a5acf49ff6bc
GuilhermeLaraRusso <guilhermelrusso@gmail.com>
2021-10-25 14:46:13 -0300

    ajustado code climate

--- desafio4.sql
+++ desafio4.sql
@@ -1,15 +1,18 @@
 -- SELECT * FROM hr.employees;
 -- SELECT * FROM hr.jobs;
 
-SELECT j.JOB_TITLE AS 'Cargo', ROUND(AVG(e.SALARY),2) AS 'Média salarial',
-CASE
-	WHEN AVG(e.SALARY) BETWEEN 2000 AND 5800 THEN 'Júnior'
-	WHEN AVG(e.SALARY) BETWEEN 5801 AND 7500 THEN 'Pleno'
-	WHEN AVG(e.SALARY) BETWEEN 7501 AND 10500 THEN 'Sênior'
-    ELSE 'CEO'
-END AS 'Senioridade'
-FROM hr.employees AS e
-INNER JOIN hr.jobs AS j
-ON e.JOB_ID = j.JOB_ID
+SELECT 
+    j.JOB_TITLE AS 'Cargo',
+    ROUND(AVG(e.SALARY), 2) AS 'Média salarial',
+    CASE
+        WHEN AVG(e.SALARY) BETWEEN 2000 AND 5800 THEN 'Júnior'
+        WHEN AVG(e.SALARY) BETWEEN 5801 AND 7500 THEN 'Pleno'
+        WHEN AVG(e.SALARY) BETWEEN 7501 AND 10500 THEN 'Sênior'
+        ELSE 'CEO'
+    END AS 'Senioridade'
+FROM
+    hr.employees AS e
+        INNER JOIN
+    hr.jobs AS j ON e.JOB_ID = j.JOB_ID
 GROUP BY Cargo
-ORDER BY ROUND(AVG(SALARY),2) ASC, Cargo;
+ORDER BY ROUND(AVG(SALARY), 2) ASC , Cargo;
--- desafio9.sql
+++ desafio9.sql
@@ -1,8 +1,9 @@
-
-
-SELECT CONCAT(e.FirstName, ' ', e.LastName) AS 'Nome completo', COUNT(o.CustomerID) AS 'Total de pedidos'
-FROM w3schools.employees AS e
-INNER JOIN w3schools.orders AS o
-ON e.EmployeeID = o.EmployeeID
+SELECT 
+    CONCAT(e.FirstName, ' ', e.LastName) AS 'Nome completo',
+    COUNT(o.CustomerID) AS 'Total de pedidos'
+FROM
+    w3schools.employees AS e
+        INNER JOIN
+    w3schools.orders AS o ON e.EmployeeID = o.EmployeeID
 GROUP BY o.EmployeeID
 ORDER BY COUNT(o.CustomerID);
