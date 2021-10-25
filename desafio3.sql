878899b5e85c7894b8385a0872ff7db34bef22b3
Guilherme Moreno <guilherme.lmoreno23@gmail.com>
2021-10-25 20:29:00 -0300

    TEST LINT

--- desafio3.sql
+++ desafio3.sql
@@ -1,6 +1,6 @@
-USE hr;
-
-SELECT JOB_TITLE AS 'Cargo',
+SELECT 
+JOB_TITLE AS 'Cargo',
 MAX_SALARY - MIN_SALARY AS 'Diferença entre salários máximo e mínimo'
-FROM jobs
-ORDER BY MIN_SALARY - MAX_SALARY, JOB_TITLE;
+FROM
+hr.jobs
+ORDER BY MAX_SALARY - MIN_SALARY, JOB_TITLE ;
