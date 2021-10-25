6f0d4a6993e1199c9a5255f6f994ae8c0808cff6
Natalia Camarinha <82239808+natcamarinha@users.noreply.github.com>
2021-10-25 17:14:25 -0300

    Update desafio4.sql

--- desafio4.sql
+++ desafio4.sql
@@ -1,6 +1,6 @@
 SELECT 
     j.JOB_TITLE AS 'Cargo',
-    ROUND(AVG(e.SALARY), 2) AS 'Média Salarial',
+    ROUND(AVG(e.SALARY), 2) AS 'Média salarial',
     CASE
         WHEN ROUND(AVG(e.SALARY), 2) BETWEEN 2000 AND 5800 THEN 'Júnior'
         WHEN ROUND(AVG(e.SALARY), 2) BETWEEN 5801 AND 7500 THEN 'Pleno'
