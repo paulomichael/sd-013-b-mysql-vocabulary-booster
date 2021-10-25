10c72316393bfed8155c088ac6c5b0ae41ad6935
marcusrodriguesdev <marcus.rodrigues10@hotmail.com>
2021-10-25 18:58:31 -0300

    test

--- desafio6.sql
+++ desafio6.sql
@@ -1,10 +1,10 @@
 SELECT 
-    CONCAT(E.FIRST_NAME, ' ', E.LAST_NAME) AS `Nome completo`,
-    J.JOB_TITLE AS `Cargo`,
-    JH.START_DATE AS `Data de início do cargo`,
-    D.DEPARTMENT_NAME AS `Departamento`
+  CONCAT(E.FIRST_NAME, ' ', E.LAST_NAME) AS `Nome completo`,
+  J.JOB_TITLE AS `Cargo`,
+  JH.START_DATE AS `Data de início do cargo`,
+  D.DEPARTMENT_NAME AS `Departamento`
 FROM
-    hr.employees AS E
+  hr.employees AS E
 		JOIN 
 	hr.job_history AS JH ON E.EMPLOYEE_ID = JH.EMPLOYEE_ID
 		JOIN
