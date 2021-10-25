c5086e7382bcf1360f7e179d5524dced1477a0d3
Ygor Lage <75700712+Yg-or@users.noreply.github.com>
2021-10-25 17:54:17 -0300

    requisito 3

--- desafio3.sql
+++ desafio3.sql
@@ -1,1 +1,1 @@
-SELECT JOB_TITLE AS `Cargo`, MAX_SALARY - MIN_SALARY AS `Diferença entre salários máximo e mínim` FROM hr.jobs ORDER BY `Diferença entre salários máximo e mínim`, JOB_TITLE ASC;
+SELECT JOB_TITLE AS `Cargo`, MAX_SALARY - MIN_SALARY AS `Diferença entre salários máximo e mínim` FROM hr.jobs ORDER BY `Diferença entre salários máximo e mínimo`, JOB_TITLE ASC;
