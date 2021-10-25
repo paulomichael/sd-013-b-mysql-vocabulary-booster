7e4c1fb9a212af9f12d170e237b51d55eb0b505a
Amós Rodrigues <amos.adm.rh@gmail.com>
2021-10-25 14:58:43 -0300

    realiza requisito 1 e refatora ordenacao

--- desafio1.sql
+++ desafio1.sql
@@ -4,4 +4,4 @@ SELECT
 		WHEN REGION_ID IN(SELECT REGION_ID FROM hr.regions WHERE REGION_NAME = "Europe") THEN 'incluído'
         ELSE 'não incluído'
         END AS 'Status Inclusão'
-FROM hr.countries;
+FROM hr.countries ORDER BY COUNTRY_NAME;
