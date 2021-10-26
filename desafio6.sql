3d5db394ca8a21d272fa3e65f4e5632c8db795a0
Aleilton Chavenco <aleilton@gmail.com>
2021-10-25 23:03:02 -0300

    Requisito 6 - correção

--- desafio6.sql
+++ desafio6.sql
@@ -1,11 +1,14 @@
-SELECT e.full_name AS `Nome completo`,
+SELECT employee_hire.full_name AS `Nome completo`,
 j.job_title AS `Cargo`,
-e.hire_date AS `Data de início do cargo`,
+employee_hire.hire_date AS `Data de início do cargo`,
 d.department_name AS `Departamento`
-FROM (SELECT CONCAT(first_name, ' ', last_name) as full_name, job_id, hire_date, department_id
-FROM hr.employees) AS e
+FROM (SELECT CONCAT(first_name, ' ', last_name) as full_name, job_id, hire_date, department_id FROM hr.employees AS e
+WHERE NOT EXISTS (
+SELECT * FROM hr.job_history
+WHERE employee_id = e.employee_id
+)) AS employee_hire
 JOIN hr.jobs AS j
-ON j.job_id = e.job_id
+ON j.job_id = employee_hire.job_id
 JOIN hr.departments AS d
-ON d.department_id = e.department_id
-ORDER BY e.full_name DESC, d.department_name ASC;
+ON d.department_id = employee_hire.department_id
+ORDER BY employee_hire.full_name DESC, d.department_name ASC;
