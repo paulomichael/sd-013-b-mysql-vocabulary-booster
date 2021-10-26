29b9d2f2361480a4006e8967ec6270261d700157
Amós Rodrigues <amos.adm.rh@gmail.com>
2021-10-26 10:15:28 -0300

    realiza requisitos 14 ao 17

---
+++ desafio16.sql
@@ -0,0 +1,15 @@
+DELIMITER $$
+CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario(email VARCHAR(50))
+RETURNS INT READS SQL DATA
+BEGIN
+	RETURN
+(SELECT 
+    COUNT(*) AS total_empregos
+FROM
+    hr.job_history job
+        INNER JOIN
+    hr.employees emp ON emp.EMPLOYEE_ID = job.EMPLOYEE_ID
+WHERE
+    emp.EMAIL = email);
+END $$
+DELIMITER ;
