4c3f48ba25f937ab94d51324b0aced2ec0053aa0
Joao Victor Cristino F <jvictorfranca@yahoo.com.br>
2021-10-25 21:58:53 -0300

    faz o 10 errado

--- desafio9.sql
+++ desafio9.sql
@@ -2,9 +2,4 @@ SELECT CONCAT(t1.FirstName, ' ', t1.LastName) AS 'Nome Completo' from employees 
 WHERE EXISTS(
 SELECT * FROM customers
 WHERE 'Nome Completo' = customers.ContactName
-)
-;
-
-SELECT CONCAT(t1.FirstName, ' ', t1.LastName) AS 'Nome Completo' from employees AS t1
-Order By  CONCAT(t1.FirstName, ' ', t1.LastName);
-SELECT * from customers;
+);
