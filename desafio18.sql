961704c0db6acf0f69e49ae01b2dcd209fb65fee
matheuscarvalhoscm <matheuscarvalhoscm@gmail.com>
2021-10-26 12:38:12 -0300

    Requisito 18

---
+++ desafio18.sql
@@ -0,0 +1,11 @@
+SELECT    
+  CONCAT(E.FIRST_NAME, ' ', E.LAST_NAME) AS `Nome completo`,  
+  CONCAT(DAY(JH.START_DATE), '/', MONTH(JH.START_DATE), '/', YEAR(JH.START_DATE)) AS `Data de início`,   
+  CONCAT(DAY(JH.END_DATE), '/', MONTH(JH.END_DATE), '/', YEAR(JH.END_DATE)) AS `Data de rescisão`,   
+  ROUND(DATEDIFF(JH.END_DATE, JH.START_DATE) / 365, 2) AS `Anos trabalhados` 
+  FROM   
+    hr.employees AS E   
+    INNER JOIN hr.job_history AS JH ON JH.EMPLOYEE_ID = E.EMPLOYEE_ID 
+ORDER BY  
+  `Nome completo` ASC,   
+  `Anos trabalhados` ASC;
