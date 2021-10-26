818cb95ad8e4b92cf4ec9ce7e074386c5f957331
Joao Victor <joaogmavictor@gmail.com>
2021-10-25 22:55:05 -0300

    Concluindo 11

--- desafio11.sql
+++ desafio11.sql
@@ -4,8 +4,7 @@ T1.Country AS `País`,
 COUNT(T2.Country) - 1 AS `Número de compatriotas`
 FROM w3schools.customers AS T1, w3schools.customers AS T2
 WHERE T1.Country = T2.Country
-GROUP BY `Nome`
+GROUP BY `Nome`, `País`
 HAVING `Número de compatriotas` > 0
 ORDER BY `Nome`;
-
 -- REF https://www.w3schools.com/sql/sql_join_self.asp
