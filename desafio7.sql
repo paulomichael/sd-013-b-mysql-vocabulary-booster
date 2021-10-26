d3ba31e9d1c0c707a26163c9fbc67915f3bd89c5
Dayvid Ruan <ruan12bbg@gmail.com>
2021-10-26 20:24:42 -0300

    Requisito 7

---
+++ desafio7.sql
@@ -0,0 +1,9 @@
+SELECT 
+    UCASE(CONCAT(E.FIRST_NAME, ' ', E.LAST_NAME)) AS 'Nome completo',
+    JH.START_DATE AS 'Data de início',
+    E.SALARY
+FROM
+    hr.employees AS E
+        INNER JOIN
+    hr.job_history AS JH ON JH.EMPLOYEE_ID = E.EMPLOYEE_ID
+ORDER BY `Nome Completo` ASC , `Data de início` ASC;
