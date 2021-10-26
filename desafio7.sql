0a96bd54680ecb2c5211f448ea9d0da67d57e12b
pedro <pedrorsehn@hotmail.com>
2021-10-26 13:33:18 -0300

    linter maldito2

--- desafio7.sql
+++ desafio7.sql
@@ -1,7 +1,4 @@
-SELECT UCASE(CONCAT(e.FIRST_NAME, ' ',e.LAST_NAME)) AS 'Nome completo', 
-jh.START_DATE AS 'Data de início',
-e.SALARY AS 'Salário'
- FROM job_history AS jh 
- INNER JOIN employees AS e ON jh.EMPLOYEE_ID = e.EMPLOYEE_ID 
+SELECT UCASE(CONCAT(e.FIRST_NAME, ' ',e.LAST_NAME)) AS 'Nome completo', jh.START_DATE AS 'Data de início', e.SALARY AS 'Salário'
+ FROM job_history AS jh INNER JOIN employees AS e ON jh.EMPLOYEE_ID = e.EMPLOYEE_ID 
  WHERE MONTH(jh.START_DATE) BETWEEN 01 AND 03 
  ORDER BY CONCAT(UCASE(e.FIRST_NAME), ' ',UCASE(e.LAST_NAME));
