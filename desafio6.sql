573468d8d97a1e4037b72bc0e644411eb9fab3ca
Dororops <leeurw@hotmail.com>
2021-10-26 02:41:34 -0300

    Adicionando req-06

---
+++ desafio6.sql
@@ -0,0 +1,14 @@
+SELECT 
+    CONCAT(FIRST_NAME, ' ', LAST_NAME) AS 'Nome completo',
+    JOB_TITLE AS 'Cargo',
+    START_DATE AS 'Data de início do cargo',
+    DEPARTMENT_NAME AS 'Departamento'
+FROM
+    hr.jobs AS J
+        JOIN
+	hr.job_history AS JH ON J.JOB_ID = JH.JOB_ID
+		JOIN
+    hr.employees AS E ON JH.EMPLOYEE_ID = E.EMPLOYEE_ID
+		JOIN
+	hr.departments AS D ON E.DEPARTMENT_ID = D.DEPARTMENT_ID
+ORDER BY `Nome completo` DESC , `Cargo` ASC;
