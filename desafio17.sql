a1df4fd4aa6781cca8a0ab4f92d219c149a2fa7a
MilenaCLeme <milenaleme4@hotmail.com>
2021-10-26 03:01:12 -0300

    Arrumando o desafio 17

--- desafio17.sql
+++ desafio17.sql
@@ -1,9 +1,9 @@
 
 DELIMITER $$
 CREATE TRIGGER trigger_inserir_data
-	BEFORE INSERT ON w3schools.orders
+	  BEFORE INSERT ON w3schools.orders
     FOR EACH ROW
 BEGIN
-	SET NEW.OrderDate = DATE(NOW());
+	  SET NEW.OrderDate = DATE(NOW());
 END $$
 DELIMITER ;
