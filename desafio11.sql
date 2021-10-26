d258847d600da0a2cda2ec77877848d250e93152
Joao Victor Cristino F <jvictorfranca@yahoo.com.br>
2021-10-26 00:41:23 -0300

    arruma o 11 finalmente de vez agora

--- desafio11.sql
+++ desafio11.sql
@@ -1,4 +1,5 @@
 SELECT c1.ContactName AS Nome, MAX(c1.Country) AS País, COUNT(c2.CustomerName) -1 AS "Número de compatriotas"
 FROM customers AS c1, customers AS c2 WHERE c2.Country = c1.Country
 GROUP BY c1.ContactName
+HAVING COUNT(c2.CustomerName) > 1
 ORDER BY c1.ContactName;
