c6d788b6d5c2b6e844a8b071446ee8132793b248
Brenojbs <brenojbs@hotmail.com>
2021-10-25 21:14:41 -0300

    Desafio 5

---
+++ desafio5.sql
@@ -0,0 +1,5 @@
+SELECT hj.JOB_TITLE AS 'Cargo', hj.MAX_SALARY - hj.MIN_SALARY AS 'Variação Salarial',
+ROUND(hj.MIN_SALARY / 12, 2) AS `Média mínima mensal`,
+ROUND(hj.MAX_SALARY / 12, 2) AS `Média máxima mensal`
+FROM jobs AS hj
+ORDER BY 'Variação Salarial' ASC, JOB_TITLE;
