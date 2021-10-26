3406e97a8772d6afb7ffc38fc4bf5a9cb59c10f1
Aleilton Chavenco <aleilton@gmail.com>
2021-10-25 21:01:34 -0300

    Requisito 5 - correção

--- desafio5.sql
+++ desafio5.sql
@@ -1,7 +1,7 @@
 SELECT j.job_title AS `Cargo`,
     ROUND((j.max_salary - j.min_salary) * 12, 2) AS `Variação Salarial`,
-    ROUND(e.min_salary * 12, 2) AS `Média mínima mensal`,
-    ROUND(e.max_salary * 12, 2) AS `Média máxima mensal`
+    ROUND(e.min_salary, 2) AS `Média mínima mensal`,
+    ROUND(e.max_salary, 2) AS `Média máxima mensal`
 FROM (SELECT job_id,
 MIN(salary) AS min_salary,
 MAX(salary) AS max_salary FROM hr.employees
