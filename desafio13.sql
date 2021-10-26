e7b005888528e61edd6ce187426b6991ab7cd917
GenesisHenriques <genesishenriques7@gmail.com>
2021-10-26 12:06:48 -0300

    .

--- desafio13.sql
+++ desafio13.sql
@@ -1,9 +1,9 @@
-SELECT
+SELECT 
     p.ProductName AS `Produto`, p.Price AS `Preço`
 FROM
     w3schools.products AS p
         INNER JOIN
     w3schools.order_details AS o ON o.ProductID = p.ProductID
 WHERE
-	o.Quantity > 80
+    o.Quantity > 80
 ORDER BY `Produto`;
