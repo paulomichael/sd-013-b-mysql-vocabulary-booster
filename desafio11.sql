2d9d08535ef527d02748f5ec7fa0f9a5bd88510e
Vinicius <viniderp@gmail.com>
2021-10-25 21:17:11 -0300

    climate fix 2

--- desafio11.sql
+++ desafio11.sql
@@ -1,14 +1,14 @@
 SELECT
-	cm.ContactName as "Nome",
-    cm.Country as "País",
-    tb.compratiots as "Número de compatriotas"
+cm.ContactName as "Nome",
+cm.Country as "País",
+tb.compratiots as "Número de compatriotas"
 FROM w3schools.customers as cm
 INNER JOIN (
-	SELECT
-		COUNT(*) - 1 as compratiots,
-        Country
-    FROM w3schools.customers
-    GROUP BY Country
+SELECT
+COUNT(*) - 1 as compratiots,
+Country
+FROM w3schools.customers
+GROUP BY Country
 ) as tb
 ON tb.Country = cm.Country
 WHERE tb.compratiots > 0
