93e54224c3f8bd77702ca3c0ce02e91aed63a5e8
Dororops <leeurw@hotmail.com>
2021-10-26 01:29:29 -0300

    Arrumando req-05

--- desafio5.sql
+++ desafio5.sql
@@ -1,11 +1,7 @@
-SELECT 
-    JOB_TITLE AS `Cargo`,
-    MAX(J.MAX_SALARY) - MIN(J.MIN_SALARY) AS `Variação Salarial`,
-    ROUND((J.MIN_SALARY) / 12, 2) AS `Média mínima mensal`,
-    ROUND((J.MAX_SALARY) / 12, 2) AS `Média máxima mensal`
-FROM
-    hr.jobs AS J
-        JOIN
-    hr.employees AS E ON J.JOB_ID = E.JOB_ID
-GROUP BY Cargo
-ORDER BY `Variação Salarial`, Cargo ASC;
+SELECT
+	JOB_TITLE AS `Cargo`,
+	MAX_SALARY - MIN_SALARY AS `Variação Salarial`,
+    ROUND(MIN_SALARY / 12, 2) AS `Média mínima mensal`,
+	ROUND(MAX_SALARY / 12, 2) AS `Média mínima mensal`
+FROM hr.jobs
+ORDER BY `Variação Salarial`, `Cargo` ASC;
