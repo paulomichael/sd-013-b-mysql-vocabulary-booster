01e5226ecf16a05a1e1129ea4a4df073c40aef8e
Brenojbs <brenojbs@hotmail.com>
2021-10-25 23:42:12 -0300

    Desafio 7

--- desafio7.sql
+++ desafio7.sql
@@ -1,5 +1,7 @@
-SELECT upper(CONCAT(he.FIRST_NAME, ' ', he.LAST_NAME)) AS `Nome Completo`, he.HIRE_DATE AS `Data de início do cargo`, he.SALARY AS `Salário`
-FROM hr.employees AS he
-INNER JOIN hr.jobs AS hj ON hj.JOB_ID = he.JOB_ID
-INNER JOIN hr.departments AS hd ON he.DEPARTMENT_ID = hd.DEPARTMENT_ID
-ORDER BY `Nome Completo`, `Data de início do cargo`;
+SELECT upper(CONCAT(he.FIRST_NAME, ' ', he.LAST_NAME)) AS `Nome completo`, hjo.start_date AS `Data de início`, he.SALARY AS `Salário`
+FROM hr.job_history AS hjo
+INNER JOIN hr.employees AS he
+ON hjo.EMPLOYEE_ID = he.EMPLOYEE_ID AND MONTH(hjo.start_date) IN (1, 2, 3)
+INNER JOIN hr.jobs AS hj ON hj.JOB_ID = hjo.JOB_ID
+INNER JOIN hr.departments AS hd ON hd.DEPARTMENT_ID = hjo.DEPARTMENT_ID
+ORDER BY `Nome completo`, `Data de início` ASC;
