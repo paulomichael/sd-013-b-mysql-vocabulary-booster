25731971e49cd3b928ce23f4beb3f2af3566624a
Brenojbs <brenojbs@hotmail.com>
2021-10-25 22:34:46 -0300

    Desafio 6(1)

--- desafio6.sql
+++ desafio6.sql
@@ -2,6 +2,6 @@ SELECT CONCAT(he.FIRST_NAME, ' ', he.LAST_NAME) AS `Nome Completo`, hj.JOB_TITLE
 FROM hr.job_history AS hjo
 INNER JOIN hr.employees AS he
 ON hjo.EMPLOYEE_ID = he.EMPLOYEE_ID
-INNER JOIN hr.jobs AS hj ON hj.JOB_ID = he.JOB_ID
-INNER JOIN hr.departments AS hd ON he.DEPARTMENT_ID = hd.DEPARTMENT_ID
-ORDER BY `Nome Completo` DESC, hj.JOB_TITLE;
+INNER JOIN hr.jobs AS hj ON hj.JOB_ID = hjo.JOB_ID
+INNER JOIN hr.departments AS hd ON hd.DEPARTMENT_ID = hjo.DEPARTMENT_ID
+ORDER BY `Nome completo` DESC, Cargo ASC;
