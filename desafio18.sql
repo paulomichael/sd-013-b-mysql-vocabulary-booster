06aa77827854550ae8f4726ad2a85b317a4d50de
matheuscarvalhoscm <matheuscarvalhoscm@gmail.com>
2021-10-26 13:11:22 -0300

    Corrige requisito 18

--- desafio18.sql
+++ desafio18.sql
@@ -1,7 +1,7 @@
 SELECT    
   CONCAT(E.FIRST_NAME, ' ', E.LAST_NAME) AS `Nome completo`,  
-  CONCAT(DAY(JH.START_DATE), '/', MONTH(JH.START_DATE), '/', YEAR(JH.START_DATE)) AS `Data de início`,   
-  CONCAT(DAY(JH.END_DATE), '/', MONTH(JH.END_DATE), '/', YEAR(JH.END_DATE)) AS `Data de rescisão`,   
+  CONCAT(DAY(JH.START_DATE), '/', DATE_FORMAT(JH.START_DATE, '%m'), '/', YEAR(JH.START_DATE)) AS `Data de início`,   
+  CONCAT(DAY(JH.END_DATE), '/',DATE_FORMAT(JH.END_DATE, '%m'), '/', YEAR(JH.END_DATE)) AS `Data de rescisão`,   
   ROUND(DATEDIFF(JH.END_DATE, JH.START_DATE) / 365, 2) AS `Anos trabalhados` 
   FROM   
     hr.employees AS E   
@@ -9,3 +9,6 @@ SELECT
 ORDER BY  
   `Nome completo` ASC,   
   `Anos trabalhados` ASC;
+
+-- ref para pegar o '0' dos meses: https://stackoverflow.com/questions/7043794/get-mysql-month-to-use-leading-zeros
+ 
