c2c472a2bb35fa05b4bcda0e7ea4a23a6a938ad9
Dororops <leeurw@hotmail.com>
2021-10-26 18:54:15 -0300

    Arrumando req-07

--- desafio7.sql
+++ desafio7.sql
@@ -1,9 +1,9 @@
 SELECT 
     UCASE(CONCAT(FIRST_NAME, ' ', LAST_NAME)) AS 'Nome completo',
-    JH.START_DATE,
+    JH.START_DATE AS 'Data de início',
     E.SALARY AS 'Salário'
 FROM
 hr.employees AS E
 JOIN hr.job_history AS JH ON E.EMPLOYEE_ID = JH.EMPLOYEE_ID
 WHERE month(JH.START_DATE) IN (01,02,03)
-ORDER BY `Nome completo` ASC;
+ORDER BY `Nome completo`, `Data de início` ASC;
