ea634b2d66530175dc136b5de82b8e4f31cebe82
MilenaCLeme <milenaleme4@hotmail.com>
2021-10-26 02:36:54 -0300

    Alteraçao para arrumar

--- desafio16.sql
+++ desafio16.sql
@@ -3,7 +3,7 @@ DELIMITER $$
 CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario(email VARCHAR(300))
 RETURNS INT READS SQL DATA
 BEGIN
-	DECLARE qt_job INT;
+    DECLARE qt_job INT;
     SELECT COUNT(jh.EMPLOYEE_ID)
     FROM hr.employees AS e 
     JOIN hr.job_history AS jh 
