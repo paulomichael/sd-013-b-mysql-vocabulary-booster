28f262c81b51f4c83dbba4246499060d097f004c
Leonardo <leo_mc_inter@hotmail.com>
2021-10-26 00:33:30 -0300

    Code Climate

--- desafio16.sql
+++ desafio16.sql
@@ -3,17 +3,17 @@ DELIMITER $$
 CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario(email VARCHAR(20))
 RETURNS INT READS SQL DATA
 BEGIN
-	DECLARE quantity INT;
-	SELECT 
-    COUNT(jh.EMPLOYEE_ID)
+DECLARE quantity INT;
+SELECT 
+COUNT(jh.EMPLOYEE_ID)
 FROM
-    hr.job_history AS jh
-        JOIN
-    hr.employees AS e ON e.EMPLOYEE_ID = jh.EMPLOYEE_ID
+hr.job_history AS jh
+JOIN
+hr.employees AS e ON e.EMPLOYEE_ID = jh.EMPLOYEE_ID
 WHERE
-    e.EMAIL = email
+e.EMAIL = email
 GROUP BY e.EMPLOYEE_ID INTO quantity;
-    RETURN quantity;
+RETURN quantity;
 END $$
 
 DELIMITER ;
