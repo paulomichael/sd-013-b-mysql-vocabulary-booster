9e2443c2b32ee56793bd24dac5e11a4a8ad0427e
unknown <sergio.espp@gmail.com>
2021-10-26 17:11:41 -0300

    Requisito 6 e 7 tentativa 3

--- desafio6.sql
+++ desafio6.sql
@@ -1,9 +1,9 @@
-SELECT CONCAT(employee.FIRST_NAME, ' ', employee.LAST_NAME) AS `Nome Completo`,
-job.JOB_TITLE AS `Cargo`,
-jh.START_DATE AS `Data de início do cargo`,
+SELECT CONCAT(employees.FIRST_NAME, ' ', employees.LAST_NAME) AS `Nome Completo`,
+jobs.JOB_TITLE AS `Cargo`,
+job_history.START_DATE AS `Data de início do cargo`,
 department.DEPARTMENT_NAME AS `Departamento`
-FROM hr.employees AS employee
-INNER JOIN hr.job_history AS jh ON jh.EMPLOYEE_ID = employee.EMPLOYEE_ID
-INNER JOIN hr.jobs AS job ON job.JOB_ID = jh.JOB_ID
-INNER JOIN hr.departments AS department ON department.DEPARTMENT_ID = jh.DEPARTMENT_ID
-ORDER BY `Nome completo` DESC , `Cargo`;
+FROM hr.job_history AS job_history
+INNER JOIN hr.employees AS employees ON job_history.EMPLOYEE_ID = employees.EMPLOYEE_ID
+INNER JOIN hr.jobs AS jobs ON jobs.JOB_ID = job_history.JOB_ID
+INNER JOIN hr.departments AS department ON department.DEPARTMENT_ID = job_history.DEPARTMENT_ID
+ORDER BY `Nome completo` DESC, `Cargo` ASC;
--- desafio7.sql
+++ desafio7.sql
@@ -1,7 +1,7 @@
 SELECT UCASE(CONCAT(employee.FIRST_NAME, ' ', employee.LAST_NAME)) AS `Nome Completo`,
 jh.START_DATE AS `Data de início`,
 employee.SALARY AS `Salário`
-FROM hr.employees AS employee
-INNER JOIN hr.job_history AS jh ON jh.EMPLOYEE_ID = employee.EMPLOYEE_ID
-WHERE  MONTH(jh.START_DATE) IN (1, 2, 3)
-ORDER BY `Nome Completo`, `Data de início`;
+FROM hr.job_history AS jh
+INNER JOIN hr.employees AS employee ON jh.EMPLOYEE_ID = employee.EMPLOYEE_ID
+WHERE MONTH(START_DATE) IN (1, 2, 3)
+ORDER BY `Nome completo` ASC, `Data de início` ASC;
