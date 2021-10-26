07fd84cabbb313cf8129fd499a95c4fd637cc5a2
JoãoBDf <joaobdf@hotmail.com>
2021-10-26 10:06:18 -0300

    CodeClimate D-14

--- desafio14.sql
+++ desafio14.sql
@@ -1,17 +1,12 @@
-SELECT
-	DISTINCT sup.Country AS `País`
+SELECT DISTINCT
+    sup.Country AS `País`
 FROM
-	(
-		SELECT
-			CustomerName, Country
-		FROM
-			w3schools.customers
-		UNION ALL
-        SELECT
-			SupplierName,
-            Country
-		FROM
-			w3schools.suppliers
-		) AS sup
+    (SELECT 
+        CustomerName, Country
+    FROM
+        w3schools.customers UNION ALL SELECT 
+        SupplierName, Country
+    FROM
+        w3schools.suppliers) AS sup
 ORDER BY Country
 LIMIT 5;
