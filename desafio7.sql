4d053bc9c8d17be5f8f8e6623adb3ce19c0956b2
Dayvid Ruan <ruan12bbg@gmail.com>
2021-10-26 20:28:46 -0300

    Corrige Requisito 7

--- desafio7.sql
+++ desafio7.sql
@@ -1,7 +1,7 @@
 SELECT 
     UCASE(CONCAT(E.FIRST_NAME, ' ', E.LAST_NAME)) AS 'Nome completo',
     JH.START_DATE AS 'Data de início',
-    E.SALARY
+    E.SALARY AS 'Salário'
 FROM
     hr.employees AS E
         INNER JOIN
