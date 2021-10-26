2d0eda86ba7fa3e32adc5766f85b43dd53f1df3a
Guilherme <guilhermerg1234@gmail.com>
2021-10-25 23:07:08 -0300

    corrigindo a 4 novamente

--- desafio4.sql
+++ desafio4.sql
@@ -1,15 +1,15 @@
 SELECT 
     JOBS.job_title AS `Cargo`,
-    ROUND(AVG(EMPLOYEES.salary), 2) AS 'Média salarial',
+    ROUND(AVG(EMPLOYEES.salary), 2) AS `Média salarial`,
     CASE
-        WHEN AVG(EMPLOYEES.salary) BETWEEN 2000 AND 5800 THEN 'Júnior'
-        WHEN AVG(EMPLOYEES.salary) BETWEEN 5801 AND 7500 THEN 'Pleno'
-        WHEN AVG(EMPLOYEES.salary) BETWEEN 7501 AND 10500 THEN 'Sênior'
-        WHEN AVG(EMPLOYEES.salary) > 10500 THEN 'CEO'
-    END AS Senioridade
+        WHEN ROUND (AVG(EMPLOYEES.salary), 2) BETWEEN 2000 AND 5800 THEN 'Júnior'
+        WHEN ROUND (AVG(EMPLOYEES.salary), 2) BETWEEN 5801 AND 7500 THEN 'Pleno'
+        WHEN ROUND (AVG(EMPLOYEES.salary), 2) BETWEEN 7501 AND 10500 THEN 'Sênior'
+        WHEN ROUND (AVG(EMPLOYEES.salary), 2) > 10500 THEN 'CEO'
+    END AS `Senioridade`
 FROM
     hr.jobs AS JOBS
         INNER JOIN
     hr.employees AS EMPLOYEES ON JOBS.JOB_ID = EMPLOYEES.JOB_ID
 GROUP BY JOBS.job_title
-ORDER BY 'Média salarial' ASC , Cargo ASC;
+ORDER BY `Média salarial` ASC , `Cargo` ASC;
