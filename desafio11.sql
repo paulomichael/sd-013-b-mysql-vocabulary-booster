51b37f5e86fbcf20b6239ed2eb10877526709a99
Lopes <gus.lcampos38@gmail.com>
2021-10-26 13:56:38 -0300

    fix

--- desafio11.sql
+++ desafio11.sql
@@ -1,9 +1,9 @@
 select 
-	c.ContactName as `Nome`,
+c.ContactName as `Nome`,
     c.Country as `País`,
-    (select count(CustomerID) - 1 from w3schools.customers where c.Country = Country) as `Número de compatriotas`
+(select count(CustomerID) - 1 from w3schools.customers where c.Country = Country) as `Número de compatriotas`
 from
-	w3schools.customers as c
+w3schools.customers as c
 where
-	(select count(CustomerID) - 1 from w3schools.customers where c.Country = Country) > 0
+(select count(CustomerID) - 1 from w3schools.customers where c.Country = Country) > 0
 order by c.ContactName;
--- desafio6.sql
+++ desafio6.sql
@@ -5,10 +5,10 @@ SELECT
     d.department_name AS `Departamento`
 FROM
     hr.employees AS e
-        INNER JOIN
+        INNER JOIN 
     hr.job_history AS jh ON e.EMPLOYEE_ID = jh.EMPLOYEE_ID
         INNER JOIN
     hr.jobs AS j ON j.job_id = jh.job_id
         INNER JOIN
     hr.departments AS d ON jh.DEPARTMENT_ID = d.DEPARTMENT_ID
-ORDER BY `Nome Completo` DESC , `Cargo`;
+ORDER BY `Nome completo` DESC, `Cargo`;
