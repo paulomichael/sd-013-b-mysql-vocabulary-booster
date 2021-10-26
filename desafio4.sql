576fddc6ac0e77add7ecb27d3cb5a4c5457478ed
Guilherme <guilhermerg1234@gmail.com>
2021-10-25 22:49:09 -0300

    vigesima tentativa

--- desafio4.sql
+++ desafio4.sql
@@ -1,6 +1,6 @@
 SELECT 
     JOBS.job_title AS `Cargo`,
-    ROUND(AVG(EMPLOYEES.salary), 2) AS Média salarial,
+    ROUND(AVG(EMPLOYEES.salary), 2) AS 'Média salarial',
     CASE
         WHEN AVG(EMPLOYEES.salary) BETWEEN 2000 AND 5800 THEN 'Júnior'
         WHEN AVG(EMPLOYEES.salary) BETWEEN 5801 AND 7500 THEN 'Pleno'
@@ -12,4 +12,4 @@ FROM
         INNER JOIN
     hr.employees AS EMPLOYEES ON JOBS.JOB_ID = EMPLOYEES.JOB_ID
 GROUP BY Cargo
-ORDER BY Média salarial , Cargo;
+ORDER BY 'Média salarial' , Cargo;
