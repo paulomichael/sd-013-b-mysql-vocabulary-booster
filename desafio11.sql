87dd661590945d80679d48cc3049d05aaa9b2e0f
Joao Victor <joaogmavictor@gmail.com>
2021-10-25 22:52:07 -0300

    Concluindo 11

--- desafio11.sql
+++ desafio11.sql
@@ -1,10 +1,11 @@
 SELECT
 T1.ContactName AS `Nome`,
 T1.Country AS `País`,
-COUNT(T2.Country) - 1
+COUNT(T2.Country) - 1 AS `Número de compatriotas`
 FROM w3schools.customers AS T1, w3schools.customers AS T2
 WHERE T1.Country = T2.Country
 GROUP BY `Nome`
+HAVING `Número de compatriotas` > 0
 ORDER BY `Nome`;
 
 -- REF https://www.w3schools.com/sql/sql_join_self.asp
