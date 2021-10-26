007dbdccebafc932994aae13c1e5903d7df2cf24
Eduardo Luiz Prando <eduardoprando159@hotmail.com>
2021-10-26 10:31:32 -0300

    climate

--- desafio8.sql
+++ desafio8.sql
@@ -4,5 +4,4 @@ ord.OrderDate AS `Data do pedido`
 FROM w3schools.orders AS ord
 INNER JOIN w3schools.customers AS cust ON ord.CustomerID = cust.CustomerID
 INNER JOIN w3schools.shippers AS ship ON  ord.ShipperID = ship.ShipperID WHERE ship.ShipperID IN (1 , 2)
-ORDER BY `Nome de contato` ASC, `Empresa que fez o envio` ASC, `Data do pedido` ASC
-;
+ORDER BY `Nome de contato` ASC, `Empresa que fez o envio` ASC, `Data do pedido` ASC;
