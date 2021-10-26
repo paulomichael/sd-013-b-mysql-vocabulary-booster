14ebd9df1d40fcb516b3efba2379f3575ffb3bc6
Guilherme Moreno <guilherme.lmoreno23@gmail.com>
2021-10-25 21:19:06 -0300

    Arrumando lint requesitos 1,2,4

--- desafio1.sql
+++ desafio1.sql
@@ -1,9 +1,8 @@
-USE hr;
-SELECT
-    COUNTRY_NAME AS 'País',
-    IF (REGION_ID = 1, 'incluído', 'não incluído') AS 'Status Inclusão'
+SELECT 
+    COUNTRY_NAME AS `País`,
+    IF(REGION_ID = 1,
+        'incluído',
+        'não incluído') AS `Status Inclusão`
 FROM
-    countries
-ORDER BY
-    COUNTRY_NAME;
-USE hr;
+    hr.countries
+ORDER BY COUNTRY_NAME;
--- desafio4.sql
+++ desafio4.sql
@@ -1,15 +1,17 @@
-use hr;
-
 SELECT 
-JOB_TITLE AS 'Cargo',
-ROUND(AVG(SALARY), 2) AS 'Média Salarial',
-CASE
-WHEN ROUND(AVG(SALARY), 2) BETWEEN 2000 AND 5800 THEN 'Júnior'
-WHEN ROUND(AVG(SALARY), 2) BETWEEN 5801 AND 7500 THEN 'Pleno'
-WHEN ROUND(AVG(SALARY), 2) BETWEEN 7501 AND 10500 THEN 'Sênior'
-WHEN ROUND(AVG(SALARY), 2) > 10500 THEN 'CEO'
-END AS 'Senioridade'
+    jbs.JOB_TITLE AS 'Cargo',
+    ROUND(AVG(empls.SALARY), 2) AS 'Média salarial',
+    CASE
+        WHEN ROUND(AVG(empls.SALARY), 2) BETWEEN 2000 AND 5800 THEN 'Júnior'
+        WHEN ROUND(AVG(empls.SALARY), 2) BETWEEN 5801 AND 7500 THEN 'Pleno'
+        WHEN ROUND(AVG(empls.SALARY), 2) BETWEEN 7501 AND 10500 THEN 'Sênior'
+        WHEN ROUND(AVG(empls.SALARY), 2) >= 10501 THEN 'CEO'
+    END AS 'Senioridade'
 FROM
-hr.employees
+    hr.jobs jbs
+        INNER JOIN
+    hr.employees empls
+ON
+    jbs.JOB_ID = empls.JOB_ID
 GROUP BY `Cargo`
-ORDER BY `Média Salarial` ASC, `Cargo` ASC;
+ORDER BY `Média Salarial` , `Cargo`;
