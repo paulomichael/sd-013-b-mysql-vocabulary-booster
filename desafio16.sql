0e0265f5ccd7e8508a81eeaa1b1d5b9951a97d18
MilenaCLeme <milenaleme4@hotmail.com>
2021-10-26 02:30:48 -0300

    Adicionar desafio 16

---
+++ desafio16.sql
@@ -0,0 +1,16 @@
+DELIMITER $$
+
+CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario(email VARCHAR(300))
+RETURNS INT READS SQL DATA
+BEGIN
+	DECLARE qt_job INT;
+    SELECT COUNT(jh.EMPLOYEE_ID)
+    FROM hr.employees AS e 
+    JOIN hr.job_history AS jh 
+    ON e.EMPLOYEE_ID = jh.EMPLOYEE_ID AND e.EMAIL = email 
+    group by jh.EMPLOYEE_ID
+    INTO qt_job;
+    RETURN qt_job;
+END $$
+
+DELIMITER ;
