fdf9dcbe404539137fcfb028586d9489c9ff2618
GenesisHenriques <genesishenriques7@gmail.com>
2021-10-25 23:30:40 -0300

    Desafio 10 pronto

---
+++ desafio10.sql
@@ -0,0 +1,13 @@
+SELECT 
+    ProductName AS `Produto`,
+    MIN(o.Quantity) AS `Mínima`,
+    MAX(o.Quantity) AS `Máxima`,
+    ROUND(AVG(o.Quantity), 2) AS `Média`
+FROM
+    w3schools.products AS p
+    INNER JOIN
+    w3schools.order_details AS o ON p.ProductID = o.ProductID
+    GROUP BY o.ProductID
+    HAVING `Média` > 20
+    ORDER BY `Média`, `Produto`;
+    
