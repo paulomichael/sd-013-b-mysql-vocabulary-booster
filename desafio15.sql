ed636124b1fd6a3a64fcb28ecbddcce0062fb0aa
Carol_Inglethe <carolinainglethe@gmail.com>
2021-10-25 23:57:04 -0300

    Desafio 15 refatora - procedure nao precisa de saida

--- desafio15.sql
+++ desafio15.sql
@@ -1,11 +1,11 @@
 DELIMITER $$
-CREATE PROCEDURE buscar_media_por_cargo(IN cargo VARCHAR(100), OUT mediaSal INT)
+CREATE PROCEDURE buscar_media_por_cargo(IN cargo VARCHAR(100))
 BEGIN
 SELECT 
 ROUND(AVG(em.`SALARY`) , 2)
 FROM hr.employees AS em
 JOIN hr.jobs AS jo
 ON em.`JOB_ID` = jo.`JOB_ID`
-WHERE jo.`JOB_TITLE` = cargo INTO mediaSal;
+WHERE jo.`JOB_TITLE` = cargo;
 END $$
 DELIMITER ;
