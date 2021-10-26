5393091156524dc6483393dcef63d18891505177
Aleilton Chavenco <aleilton@gmail.com>
2021-10-26 18:56:16 -0300

    Requisito 19

---
+++ desafio19.sql
@@ -0,0 +1,18 @@
+USE hr;
+DELIMITER $$
+
+CREATE FUNCTION exibir_quantidade_pessoas_contratadas_por_mes_e_ano(mes INT, ano INT)
+RETURNS INT READS SQL DATA
+BEGIN
+    DECLARE employees_total INT;
+SELECT 
+    COUNT(*)
+FROM
+    hr.employees
+WHERE
+    MONTH(HIRE_DATE) = mes
+        AND YEAR(HIRE_DATE) = ano INTO employees_total;
+    RETURN employees_total;
+END $$
+
+DELIMITER ;
