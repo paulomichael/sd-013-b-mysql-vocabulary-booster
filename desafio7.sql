0c321df2ba96bca28bc05dd90edde9376843af4e
Guilherme Fonseca <guilherme.fonseca@paco.local>
2021-10-26 13:10:26 -0300

    Desafio 07

---
+++ desafio7.sql
@@ -0,0 +1,7 @@
+SELECT 
+UPPER(CONCAT(T1.FIRST_NAME, ' ',T1.LAST_NAME)) AS 'Nome completo',
+T2.START_DATE AS 'Data de início',
+T1.SALARY AS 'Salário'
+FROM hr.employees AS T1
+INNER JOIN hr.job_history AS T2 ON T1.EMPLOYEE_ID = T2.EMPLOYEE_ID
+ORDER BY CONCAT(T1.FIRST_NAME, ' ',T1.LAST_NAME), 'Data de início';
