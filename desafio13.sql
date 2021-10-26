7624d4839bff5e3cfafa6ba210b9c73fb916d5b9
Carol_Inglethe <carolinainglethe@gmail.com>
2021-10-25 23:22:50 -0300

    Desafio 13- procura referencia em 2 tabelas e maior quantidade de pedido

---
+++ desafio13.sql
@@ -0,0 +1,8 @@
+SELECT 
+`ProductName` AS `Produto`,
+`Price` AS `Preço`
+FROM w3schools.products AS P
+JOIN w3schools.order_details AS OD
+ON P.`ProductID` = OD.`ProductID`
+WHERE OD.`Quantity` > 80
+ORDER BY `Produto`;
