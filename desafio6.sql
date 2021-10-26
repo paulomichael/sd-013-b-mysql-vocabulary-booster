fcb337211b53238567f795af99a00adbdad0dd3c
EddieRAM <edusacarneiro@gmail.com>
2021-10-26 12:31:05 -0300

    req 6 - solved ambiguous interpretation of the req

--- desafio6.sql
+++ desafio6.sql
@@ -1,12 +1,14 @@
-SELECT
+SELECT 
     CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) AS 'Nome completo',
-    j.JOB_TITLE AS Cargo,
-    e.HIRE_DATE AS 'Data de início do cargo',
+    j.JOB_TITLE AS 'Cargo',
+    jh.START_DATE AS 'Data de início do cargo',
     d.DEPARTMENT_NAME AS 'Departamento'
 FROM
-    hr.employees e
+    hr.jobs j
         INNER JOIN
-    hr.jobs j ON j.JOB_ID = e.JOB_ID
+    hr.job_history jh ON j.JOB_ID = jh.JOB_ID
         INNER JOIN
-    hr.departments d ON d.DEPARTMENT_ID = e.DEPARTMENT_ID
+    hr.employees e ON jh.EMPLOYEE_ID = e.EMPLOYEE_ID
+        INNER JOIN
+    hr.departments d ON jh.DEPARTMENT_ID = d.DEPARTMENT_ID
 ORDER BY CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) DESC , j.JOB_TITLE;
