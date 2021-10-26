8874f0cbe962f03e5cc951864007a98cacbd041b
Joao Victor <joaogmavictor@gmail.com>
2021-10-25 21:30:12 -0300

    Arrumando Code Climate

--- desafio17.sql
+++ desafio17.sql
@@ -2,9 +2,9 @@ USE w3schools
 DELIMITER $$
 
 CREATE TRIGGER insertOrderDataInOrders
-	BEFORE INSERT ON w3schools.orders
-    FOR EACH ROW
+BEFORE INSERT ON w3schools.orders
+FOR EACH ROW
 BEGIN
-	SET NEW.OrderDate = CURRENT_DATE();
+SET NEW.OrderDate = CURRENT_DATE();
 END $$
 DELIMITER ;
