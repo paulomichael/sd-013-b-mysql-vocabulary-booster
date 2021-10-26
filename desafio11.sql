3ac2ca4bd68e784820a9d62846ed58b3199dbe13
Joao Victor Cristino F <jvictorfranca@yahoo.com.br>
2021-10-26 00:19:51 -0300

    tenta arrumar o 11 a terceira vez

--- desafio11.sql
+++ desafio11.sql
@@ -1,4 +1,4 @@
-SELECT c1.CustomerName AS Nome, c1.Country AS País, COUNT(c2.CustomerName) -1 AS "Número de compatriotas"
+SELECT c1.CustomerName AS Nome, MAX(c1.Country) AS País, COUNT(c2.CustomerName) -1 AS "Número de compatriotas"
 FROM customers AS c1, customers AS c2 WHERE c2.Country = c1.Country
-GROUP BY c1.CustomerName, c1.Country
+GROUP BY c1.CustomerName
 ORDER BY c1.ContactName;
