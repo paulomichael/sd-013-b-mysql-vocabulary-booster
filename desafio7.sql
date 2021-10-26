39198955f741f2a0e776e883e056cad6369f60c5
Letícia Castro <aicitelks@gmail.com>
2021-10-26 09:29:30 -0300

    Desafio 7, 0k (evaluator)

--- desafio7.sql
+++ desafio7.sql
@@ -1,7 +1,8 @@
 SELECT
-  UCASE(CONCAT(FIRST_NAME, ' ', LAST_NAME)) AS `Nome completo`,
-  HIRE_DATE `Data de início`,
-  SALARY AS 'Salário'
-FROM hr.employees
-WHERE MONTH(HIRE_DATE) IN (1, 2, 3)
+  UCASE(CONCAT(E.FIRST_NAME, ' ', E.LAST_NAME)) AS `Nome completo`,
+  JH.START_DATE `Data de início`,
+  E.SALARY AS 'Salário'
+FROM hr.employees AS E
+INNER JOIN hr.job_history AS JH ON E.EMPLOYEE_ID = JH.EMPLOYEE_ID
+WHERE MONTH(JH.START_DATE) IN (1, 2, 3)
 ORDER BY `Nome completo`, `Data de início`;
