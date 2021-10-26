704edbe3a65c5e31c268389c96c557b485375c7a
Dororops <leeurw@hotmail.com>
2021-10-26 18:38:41 -0300

    Adicionando req-07-02

--- desafio7.sql
+++ desafio7.sql
@@ -3,6 +3,6 @@ SELECT
     JH.START_DATE AS 'Data de início',
     E.SALARY AS 'Salário'
 FROM
-	hr.employees AS E
-	JOIN hr.job_history AS JH ON E.EMPLOYEE_ID = JH.EMPLOYEE_ID
+hr.employees AS E
+JOIN hr.job_history AS JH ON E.EMPLOYEE_ID = JH.EMPLOYEE_ID
 ORDER BY `Nome completo`, `Data de início` ASC;
