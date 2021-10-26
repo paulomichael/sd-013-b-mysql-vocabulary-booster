859c354b97e4728059242adfa23b73f1cf12cab0
pedro <pedrorsehn@hotmail.com>
2021-10-26 12:23:12 -0300

    arruma algumas questoes

--- desafio6.sql
+++ desafio6.sql
@@ -3,6 +3,6 @@ j.JOB_TITLE AS 'Cargo',
 jh.START_DATE AS 'Data de início do cargo',
 d.DEPARTMENT_NAME AS 'Departamento'
 FROM employees AS e INNER JOIN job_history as jh ON e.EMPLOYEE_ID = jh.EMPLOYEE_ID
-INNER JOIN jobs AS j ON e.JOB_ID = j.JOB_ID
-INNER JOIN departments as d ON e.DEPARTMENT_ID = d.DEPARTMENT_ID
+INNER JOIN jobs AS j ON jh.JOB_ID = j.JOB_ID
+INNER JOIN departments as d ON jh.DEPARTMENT_ID = d.DEPARTMENT_ID
 ORDER BY CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) DESC, Cargo; 
