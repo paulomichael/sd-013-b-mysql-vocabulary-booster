c87e53ab175299bcc2a14068ebdcc0e95b0ed55d
Mariana Garcia <maridgarcia045@gmail.com>
2021-10-26 19:19:30 -0300

    Requisito 8

---
+++ desafio8.sql
@@ -0,0 +1,19 @@
+SELECT 
+    (SELECT 
+            ContactName
+        FROM
+            w3schools.customers
+        WHERE
+            CustomerID = w3schools.orders.CustomerID) AS 'Nome de contato',
+    (SELECT 
+            ShipperName
+        FROM
+            w3schools.shippers
+        WHERE
+            ShipperID = w3schools.orders.ShipperID) AS 'Empresa que fez o envio',
+    OrderDate AS 'Data do pedido'
+FROM
+    w3schools.orders
+WHERE
+    ShipperID IN (1 , 2)
+ORDER BY `Nome de contato` ASC , `Empresa que fez o envio` ASC , `Data do pedido` ASC;
