4c90a8b91b40b9519c5580f82043a46c0c843704
FilipeGuto <filipeguto95@gmail.com>
2021-10-26 11:11:32 -0300

    Desafio 13

---
+++ desafio13.sql
@@ -0,0 +1,9 @@
+SELECT
+ProductName AS `Produto`,
+Price AS `Preço` 
+FROM 
+w3schools.products AS P
+INNER JOIN w3schools.order_details AS O
+ON P.ProductID = O.ProductID
+WHERE O.Quantity > 80
+ORDER BY `Produto`;
