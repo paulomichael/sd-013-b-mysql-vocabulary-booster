68b0cae9ef97a8f9bf43604aee82f80d2e7fa1c9
Camila Dornas <camiladornas@gmail.com>
2021-10-25 21:55:59 -0300

    Corrige requisito 9

--- desafio9.sql
+++ desafio9.sql
@@ -1,11 +1,9 @@
-SELECT 
+SELECT
     CONCAT(e.FirstName, ' ', e.LastName) AS 'Nome completo',
-    od.Quantity AS 'Total de pedidos'
+    COUNT(o.OrderID) AS 'Total de pedidos'
 FROM
     w3schools.employees AS e
         INNER JOIN
     w3schools.orders AS o ON o.EmployeeID = e.EmployeeID
-        INNER JOIN
-    w3schools.order_details AS od ON od.OrderID = o.OrderID
 GROUP BY `Nome completo`
 ORDER BY `Total de pedidos`;
