ebb9ff32c32c7325e93b617ffe05200588af686a
Mariana Garcia <maridgarcia045@gmail.com>
2021-10-26 08:31:39 -0300

    Requisito 5 - corrigido

--- desafio5.sql
+++ desafio5.sql
@@ -3,4 +3,4 @@ MAX_SALARY - MIN_SALARY AS 'Variação salarial',
 ROUND(MIN_SALARY/12, 2) AS 'Média mínima mensal',
 ROUND(MAX_SALARY/12, 2) AS 'Média máxima mensal'
 FROM hr.jobs
-ORDER BY MAX_SALARY - MIN_SALARY, JOB_TITLE;
+ORDER BY MAX_SALARY - MIN_SALARY, Cargo;
