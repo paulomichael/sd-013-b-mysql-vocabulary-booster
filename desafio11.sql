d8249219eefc431fa9a5d00e37f981cd0e48495c
Camila Dornas <camiladornas@gmail.com>
2021-10-25 22:19:11 -0300

    Requisito 11 corirgido

--- desafio11.sql
+++ desafio11.sql
@@ -1,9 +1,11 @@
 SELECT 
-	c1.ContactName AS 'Nome',
+    c1.ContactName AS 'Nome',
     c1.Country AS 'País',
     COUNT(c1.Country) AS 'Número de compatriotas'
-FROM 
-	w3schools.customers AS c1, w3schools.customers AS c2
-WHERE c1.Country = c2.Country
+FROM
+    w3schools.customers AS c1,
+    w3schools.customers AS c2
+WHERE
+    c1.Country = c2.Country
 GROUP BY `Nome`
 ORDER BY `Nome`;
