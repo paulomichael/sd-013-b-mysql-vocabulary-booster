e680bb0602667c6061280b1621f9d49ec9d21c88
Henrique <henriquecursino46@gmail.com>
2021-10-26 15:43:11 -0300

    Resolvendo erros code climate

--- desafio14.sql
+++ desafio14.sql
@@ -1,10 +1,10 @@
 SELECT
-	Country AS 'País'
+Country AS 'País'
 FROM
-	w3schools.customers
+w3schools.customers
 UNION SELECT
-	Country
+Country
 FROM
-	w3schools.suppliers
+w3schools.suppliers
 ORDER BY 'País'
 LIMIT 5;
