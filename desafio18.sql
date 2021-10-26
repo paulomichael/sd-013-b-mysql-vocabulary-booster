3fa1ea409973f171228aca2c44e55a9d2da57b66
matheuscarvalhoscm <matheuscarvalhoscm@gmail.com>
2021-10-26 13:21:17 -0300

    Corrige requisito 18 (?)

--- desafio18.sql
+++ desafio18.sql
@@ -1,7 +1,7 @@
 SELECT    
   CONCAT(E.FIRST_NAME, ' ', E.LAST_NAME) AS `Nome completo`,  
-  CONCAT(DAY(JH.START_DATE), '/', DATE_FORMAT(JH.START_DATE, '%m'), '/', YEAR(JH.START_DATE)) AS `Data de início`,   
-  CONCAT(DAY(JH.END_DATE), '/',DATE_FORMAT(JH.END_DATE, '%m'), '/', YEAR(JH.END_DATE)) AS `Data de rescisão`,   
+  DATE_FORMAT(JH.START_DATE, '%d/%m/%Y') AS `Data de início`,   
+  DATE_FORMAT(JH.END_DATE, '%d/%m/%Y') AS `Data de rescisão`,   
   ROUND(DATEDIFF(JH.END_DATE, JH.START_DATE) / 365, 2) AS `Anos trabalhados` 
   FROM   
     hr.employees AS E   
