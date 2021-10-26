a812232d1eb8eb689061fbeb4ca3ce6dae0d5b0b
Alexandre Altgauzen <82180840+altgauzen@users.noreply.github.com>
2021-10-26 22:37:42 +0100

    desafio 7 refat e desafio 8 finalizado

--- desafio7.sql
+++ desafio7.sql
@@ -1,4 +1,4 @@
-SELECT CONCAT(employees.FIRST_NAME, ' ', employees.LAST_NAME) AS `Nome completo`,
+SELECT UCASE(CONCAT(employees.FIRST_NAME, ' ', employees.LAST_NAME)) AS `Nome completo`,
 job_history.START_DATE AS `Data de início`,
 employees.SALARY AS `Salário`
 FROM hr.job_history AS job_history
