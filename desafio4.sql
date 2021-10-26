7f9be64463f8302394d902caa89e71fdb97bc7a3
mariananbp <mariananbp@gmail.com>
2021-10-25 21:04:44 -0300

    Challenge 4 - 2

--- desafio4.sql
+++ desafio4.sql
@@ -1,27 +1,12 @@
-SELECT 
-    j.JOB_TITLE AS `Cargo`,
-    ROUND(AVG(SALARY), 2) AS Média salarial,
-    CASE
-        WHEN
-            ROUND(AVG(SALARY), 2) >= 2000
-                && ROUND(AVG(SALARY), 2) <= 5800
-        THEN
-            'Júnior'
-        WHEN
-            ROUND(AVG(SALARY), 2) >= 5801
-                && ROUND(AVG(SALARY), 2) <= 7500
-        THEN
-            'Pleno'
-        WHEN
-            ROUND(AVG(SALARY), 2) >= 7501
-                && ROUND(AVG(SALARY), 2) <= 10000
-        THEN
-            'Sênior'
-        ELSE 'CEO'
-    END AS Senioridade
-FROM
-    hr.employees AS e
-        INNER JOIN
-    hr.jobs AS j ON e.JOB_ID = j.JOB_ID
+SELECT j.JOB_TITLE AS `Cargo`, ROUND(AVG(SALARY), 2) AS `Média salarial`,
+CASE
+	WHEN ROUND(AVG(SALARY), 2) >= 2000 && ROUND(AVG(SALARY), 2) <= 5800 THEN 'Júnior'
+    WHEN ROUND(AVG(SALARY), 2) >= 5801 && ROUND(AVG(SALARY), 2) <= 7500 THEN 'Pleno'
+    WHEN ROUND(AVG(SALARY), 2) >= 7501 && ROUND(AVG(SALARY), 2) <= 10000 THEN 'Sênior'
+    ELSE 'CEO'
+END AS `Senioridade`
+FROM hr.employees AS e
+INNER JOIN hr.jobs AS j
+ON e.JOB_ID = j.JOB_ID
 GROUP BY JOB_TITLE
-ORDER BY Média salarial , JOB_TITLE;
+ORDER BY `Média salarial`, JOB_TITLE;
