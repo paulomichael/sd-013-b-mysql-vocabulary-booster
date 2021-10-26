7bcf187915ef36f6db24144a486ab9b8e89379bd
Henrique Lima <hnamix@gmail.com>
2021-10-26 12:36:08 -0400

    Re10 e 11 e ajuste no 9

--- desafio9.sql
+++ desafio9.sql
@@ -2,6 +2,6 @@ SELECT CONCAT(e.FirstName,' ',e.LastName) AS 'Nome completo',
 COUNT(*) AS 'Total de pedidos'
 FROM w3schools.orders AS o 
 INNER JOIN w3schools.employees AS e
-ON e.EmployeeID = o.EmployeeID
+ON o.EmployeeID = e.EmployeeID
 GROUP BY 'Nome completo'
-ORDER BY `Total de pedidos` ASC;
+ORDER BY `Total de pedidos`;
