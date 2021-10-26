8fe3906467e3268992acd24e78b4640c1d2f8acc
Mariana Garcia <maridgarcia045@gmail.com>
2021-10-26 09:23:16 -0300

    Requisito 6

---
+++ desafio6.sql
@@ -0,0 +1,12 @@
+SELECT CONCAT(e.first_name, ' ', e.last_name) AS 'Nome completo',
+j.JOB_TITLE AS Cargo,
+jh.START_DATE AS 'Data de início do cargo',
+d.DEPARTMENT_NAME AS Departamento
+FROM hr.job_history AS jh
+INNER JOIN hr.employees AS e 
+ON jh.EMPLOYEE_ID = e.EMPLOYEE_ID
+INNER JOIN hr.jobs AS j
+ON j.JOB_ID = j.JOB_ID
+INNER JOIN hr.departments AS d 
+ON jh.DEPARTMENT_ID = d.DEPARTMENT_ID
+ORDER BY 'Nome completo' ASC, Cargo DESC;
