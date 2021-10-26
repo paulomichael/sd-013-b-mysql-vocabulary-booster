c83cd701095d9e81899be20b35837fa9f066d839
Joao Victor Cristino F <jvictorfranca@yahoo.com.br>
2021-10-25 22:24:06 -0300

    resolve o 9

--- desafio9.sql
+++ desafio9.sql
@@ -1,5 +1,9 @@
-SELECT CONCAT(t1.FirstName, ' ', t1.LastName) AS 'Nome Completo' from employees AS t1
+SELECT CONCAT(t1.FirstName, ' ', t1.LastName) AS 'Nome Completo', COUNT(t2.employeeID) AS `Total de pedidos`, t1.employeeID, t2.* from employees AS t1
+INNER JOIN orders as t2 
+ON t1.employeeID = t2.employeeID
 WHERE EXISTS(
-SELECT * FROM customers
-WHERE 'Nome Completo' = customers.ContactName
-);
+SELECT * FROM orders
+WHERE  t1.employeeID = orders.employeeID
+)
+GROUP BY t1.employeeID
+ORDER BY COUNT(t2.employeeID);
