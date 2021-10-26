782072aec2d1822e0925efee2bc879f8fdfaba25
Dororops <leeurw@hotmail.com>
2021-10-25 23:52:30 -0300

    Adicionando req-04

---
+++ desafio4.sql
@@ -0,0 +1,12 @@
+SELECT 
+    JOB_TITLE AS Cargo,
+    ROUND((MAX_SALARY + MIN_SALARY) / 2, 2) AS 'Média salarial',
+    CASE
+        WHEN (MAX_SALARY + MIN_SALARY) / 2 BETWEEN 2000 AND 5800 THEN 'Júnior'
+        WHEN (MAX_SALARY + MIN_SALARY) / 2 BETWEEN 5801 AND 7500 THEN 'Pleno'
+        WHEN (MAX_SALARY + MIN_SALARY) / 2 BETWEEN 7501 AND 10500 THEN 'Sênior'
+        WHEN (MAX_SALARY + MIN_SALARY) / 2 > 10500 THEN 'CEO'
+    END AS Nível
+FROM
+    hr.jobs
+ORDER BY `Média salarial` , `Cargo` ASC;
