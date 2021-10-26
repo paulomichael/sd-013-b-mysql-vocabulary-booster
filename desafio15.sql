b33cade95985b28aa4c4d92b0184a3b7e8cc4ade
Natalia Camarinha <natcamarinha@gmail.com>
2021-10-26 18:33:00 -0300

    correção codeclimate requisito 15

--- desafio15.sql
+++ desafio15.sql
@@ -1,8 +1,8 @@
 CREATE PROCEDURE buscar_media_por_cargo(IN cargo VARCHAR(100))
 BEGIN
-	SELECT ROUND(AVG(e.SALARY), 2) AS 'Média salarial' FROM employees AS e
-    INNER JOIN jobs AS j ON j.JOB_ID = e.JOB_ID
-    WHERE cargo = j.JOB_TITLE;
+SELECT ROUND(AVG(e.SALARY), 2) AS 'Média salarial' FROM employees AS e
+INNER JOIN jobs AS j ON j.JOB_ID = e.JOB_ID
+WHERE cargo = j.JOB_TITLE;
 END $$
 
 DELIMITER ;
