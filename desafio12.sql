cdb9501baaedeab646df5fe10dda62c17160d2f8
Jeferson Oliveira <jeferson.contas@gmail.com>
2021-10-26 00:03:51 -0300

    lint 12

--- desafio12.sql
+++ desafio12.sql
@@ -1,8 +1,8 @@
-SELECT
-    CONCAT(emp1.FIRSTNAME, ' ', emp1.LASTNAME) AS 'Nome completo funcionário 1',
+SELECT 
+    CONCAT(emp1.FIRST_NAME, ' ', emp1.LAST_NAME) AS 'Nome completo funcionário 1',
     emp1.SALARY AS 'Salário funcionário 1',
     emp1.PHONE_NUMBER AS 'Telefone funcionário 1',
-    CONCAT(emp2.FIRSTNAME, ' ', emp2.LASTNAME) AS 'Nome completo funcionário 2',
+    CONCAT(emp2.FIRST_NAME, ' ', emp2.LAST_NAME) AS 'Nome completo funcionário 2',
     emp2.SALARY AS 'Salário funcionário 2',
     emp2.PHONE_NUMBER AS 'Telefone funcionário 2'
 FROM
@@ -10,5 +10,5 @@ FROM
     hr.employees AS emp2
 WHERE
     emp1.JOB_ID = emp2.JOB_ID
-        AND CONCAT(emp1.FIRSTNAME, ' ', emp1.LASTNAME) <> CONCAT(emp2.FIRSTNAME, ' ', emp2.LASTNAME)
+        AND CONCAT(emp1.FIRST_NAME, ' ', emp1.LAST_NAME) <> CONCAT(emp2.FIRST_NAME, ' ', emp2.LAST_NAME)
 ORDER BY `Nome completo funcionário 1` ASC , `Nome completo funcionário 2` ASC;
