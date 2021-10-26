61b682001ed7196d636f0a7aa40afd790aa81ef0
Natalia Camarinha <natcamarinha@gmail.com>
2021-10-26 18:15:06 -0300

    requisito 14 e correção codeclimate requisito 11

--- desafio11.sql
+++ desafio11.sql
@@ -1,7 +1,12 @@
-SELECT c1.ContactName AS `Nome`,
+SELECT 
+    c1.ContactName AS `Nome`,
     c1.Country AS `País`,
     COUNT(c2.Country) AS `Número de compatriotas`
-FROM w3schools.customers AS c1, w3schools.customers AS c2
-WHERE c1.Country = c2.Country
-GROUP BY c1.ContactName, c1.Country 
+FROM
+    w3schools.customers AS c1,
+    w3schools.customers AS c2
+WHERE
+    c1.Country = c2.Country
+GROUP BY c1.ContactName , c1.Country
 ORDER BY `Nome`;
+
