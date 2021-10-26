bb4798c726668f27b600ab8de675caba310283ef
JoãoBDf <joaobdf@hotmail.com>
2021-10-26 10:00:24 -0300

    Desafio 14 finalizado

---
+++ desafio14.sql
@@ -0,0 +1,17 @@
+SELECT
+	DISTINCT sup.Country AS `País`
+FROM
+	(
+		SELECT
+			CustomerName, Country
+		FROM
+			w3schools.customers
+		UNION ALL
+        SELECT
+			SupplierName,
+            Country
+		FROM
+			w3schools.suppliers
+		) AS sup
+ORDER BY Country
+LIMIT 5;
