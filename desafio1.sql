1fb77ffcebc1e175691a7229fd982fd0efaa304a
paulrpn <paulrpn@live.com>
2021-10-26 01:04:16 -0300

    desafio 1 com apenas uma query

--- desafio1.sql
+++ desafio1.sql
@@ -1,10 +1,19 @@
 # 1 - Exiba os países, indicando se cada um deles se encontra ou não na região formada pela Europa
 
-SET @region_id = (SELECT REGION_ID FROM hr.regions WHERE REGION_NAME = 'Europe');
+# SET @region_id = (SELECT REGION_ID FROM hr.regions WHERE REGION_NAME = 'Europe');
+
+# SELECT 
+   -- COUNTRY_NAME AS País,
+   -- IF(REGION_ID = @region_id,
+   --     'incluído',
+   --      'não incluído') AS 'Status Inclusão'
+-- FROM
+   -- hr.countries
+-- ORDER BY COUNTRY_NAME;
 
 SELECT 
     COUNTRY_NAME AS País,
-    IF(REGION_ID = @region_id,
+    IF(REGION_ID = 1,
         'incluído',
         'não incluído') AS 'Status Inclusão'
 FROM
