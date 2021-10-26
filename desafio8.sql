090fa5a8f29ff9cffb804093965889ab2b788a35
Eduardo Luiz Prando <eduardoprando159@hotmail.com>
2021-10-26 10:27:12 -0300

    Challenge 8

---
+++ desafio8.sql
@@ -0,0 +1,8 @@
+SELECT ContactName AS `Nome de contato`,
+ShipperName AS `Empresa que fez o envio`,
+ord.OrderDate AS `Data do pedido`
+FROM w3schools.orders AS ord
+INNER JOIN w3schools.customers AS cust ON ord.CustomerID = cust.CustomerID
+INNER JOIN w3schools.shippers AS ship ON  ord.ShipperID = ship.ShipperID WHERE ship.ShipperID IN (1 , 2)
+ORDER BY `Nome de contato` ASC, `Empresa que fez o envio` ASC, `Data do pedido` ASC
+;
