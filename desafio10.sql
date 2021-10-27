f0cb3b7751c930eb7476c6e6622ec4a7ded7fc6a
Dororops <leeurw@hotmail.com>
2021-10-27 02:57:32 -0300

    Adicionando req-10

---
+++ desafio10.sql
@@ -0,0 +1,8 @@
+SELECT ProductName AS 'Produto',
+MIN(OD.Quantity) AS 'Mínima',
+MAX(OD.Quantity) AS 'Máxima',
+AVG(OD.Quantity) AS 'Média'
+FROM w3schools.products AS P
+JOIN w3schools.order_details AS OD ON P.ProductID = OD.ProductID
+GROUP BY ProductName
+HAVING `Média` > 20;
