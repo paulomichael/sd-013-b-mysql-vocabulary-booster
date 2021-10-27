199c7f6bc33bfdcd9112d54d1febdcb26df7227a
Dororops <leeurw@hotmail.com>
2021-10-27 03:00:40 -0300

    Adicionando req-10-LINT

--- desafio10.sql
+++ desafio10.sql
@@ -5,4 +5,5 @@ AVG(OD.Quantity) AS 'Média'
 FROM w3schools.products AS P
 JOIN w3schools.order_details AS OD ON P.ProductID = OD.ProductID
 GROUP BY ProductName
-HAVING `Média` > 20;
+HAVING `Média` > 20
+ORDER BY `Média`, `Produto` ASC;
