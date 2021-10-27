44669572cbf1c11b05c6951e674fd544f98e27e1
Henrique Lima <hnamix@gmail.com>
2021-10-27 10:37:09 -0400

    Ajustando o codigo Re9

--- desafio9.sql
+++ desafio9.sql
@@ -1,7 +1,7 @@
 SELECT CONCAT(e.FirstName,' ',e.LastName) AS 'Nome completo',
-COUNT(o.OrderID) AS 'Total de pedidos'
+COUNT(*) AS 'Total de pedidos'
 FROM w3schools.orders AS o 
-JOIN w3schools.employees AS e
+INNER JOIN w3schools.employees AS e
 ON o.EmployeeID = e.EmployeeID
 GROUP BY `Nome completo`
 ORDER BY `Total de pedidos`;
