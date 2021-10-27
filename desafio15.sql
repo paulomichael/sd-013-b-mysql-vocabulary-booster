313e054686bf2801251ec43be7d195c53882848f
Lopes <gus.lcampos38@gmail.com>
2021-10-27 11:53:39 -0300

    fix

--- desafio15.sql
+++ desafio15.sql
@@ -1,9 +1,9 @@
 DELIMITER $$
 CREATE PROCEDURE buscar_media_por_cargo(in cargo VARCHAR(35))
 begin
-	select
-		round(avg(salary),2) as `Média salarial` from hr.employees as e
+select
+	round(avg(salary),2) as `Média salarial` from hr.employees as e
         join
-			jobs as j on e.job_id = j.job_id where cargo = j.job_title;
+	jobs as j on e.job_id = j.job_id where cargo = j.job_title;
 END $$
 DELIMITER ;
