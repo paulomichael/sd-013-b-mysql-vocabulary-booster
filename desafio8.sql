cd93fbcf158fc1d8c4024b6c2b398b77ac11c14f
Dororops <leeurw@hotmail.com>
2021-10-26 22:14:18 -0300

    Adicionando req-08

---
+++ desafio8.sql
@@ -0,0 +1,9 @@
+SELECT
+	ContactName AS 'Nome de contato',
+	S.ShipperName AS 'Empresa que fez o envio',
+    O.OrderDate AS 'Data do pedido'
+FROM w3schools.customers AS C
+	join w3schools.orders AS O ON C.CustomerID = O.CustomerID
+    JOIN w3schools.shippers AS S ON O.ShipperID = S.ShipperID
+WHERE O.ShipperID IN (1,2)
+ORDER BY `Nome de contato`, `Empresa que fez o envio`, `Data do pedido` ASC;
