d14c7ae513874ae25aff3d200ce7f905d99f8c48
Letícia Castro <aicitelks@gmail.com>
2021-10-27 09:00:44 -0300

    Desafio 8, 0k

---
+++ desafio8.sql
@@ -0,0 +1,8 @@
+SELECT
+  C.CustomerName AS `Nome de contato`,
+  SH.ShipperName AS `Empresa que fez o envio`,
+  O.OrderDate AS `Data do pedido`
+FROM w3schools.orders AS O
+INNER JOIN w3schools.customers AS C ON O.CustomerID = C.CustomerID
+INNER JOIN w3schools.shippers AS SH ON O.ShipperID = SH.ShipperID
+ORDER BY `Nome de contato`, `Empresa que fez o envio`, `Data do pedido`;
