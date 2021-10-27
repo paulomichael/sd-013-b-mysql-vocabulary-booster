03f9012f3da1900b000ab2d3ae4488e8e5fb1247
João Bruno Vasconcelos <joaobruno91@gmail.com>
2021-10-27 00:25:28 -0300

    Adiciona desafi 7

---
+++ desafio7.sql
@@ -0,0 +1,15 @@
+SELECT 
+    CONCAT(UCASE(em.FIRST_NAME),
+            ' ',
+            UCASE(em.LAST_NAME)) AS 'Nome Completo',
+    jh.START_DATE AS 'Data de início',
+    em.SALARY AS 'Salário'
+FROM
+    hr.employees AS em
+        INNER JOIN
+    hr.job_history AS jh ON em.EMPLOYEE_ID = jh.EMPLOYEE_ID
+WHERE
+    MONTH(jh.START_DATE) = 1
+        OR MONTH(jh.START_DATE) = 2
+        OR MONTH(jh.START_DATE) = 3
+ORDER BY `Nome completo` , `Data de início`;
