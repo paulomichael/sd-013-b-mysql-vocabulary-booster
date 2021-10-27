9a4a445c9ef010f218842402e2ede0a38e09ce69
Guilherme <guilhermerg1234@gmail.com>
2021-10-27 14:01:20 -0300

    corrigindo ex 5

--- desafio5.sql
+++ desafio5.sql
@@ -3,8 +3,7 @@ SELECT
     J.max_salary - J.min_salary AS `Variação Salarial`,
     ROUND(J.min_salary / 12, 2) AS `Média mínima mensal`,
     ROUND(J.max_salary / 12, 2) AS `Média máxima mensal`
-FROM
-	employees AS E
+FROM employees AS E
 INNER JOIN jobs AS J
 ON E.job_id = J.job_id
 GROUP BY E.job_id
