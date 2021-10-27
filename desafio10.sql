6b349b4a6310b64d24d9949c51b04c47570c2d28
Ygor Lage <75700712+Yg-or@users.noreply.github.com>
2021-10-27 19:03:32 -0300

    requisito 10

---
+++ desafio10.sql
@@ -0,0 +1,6 @@
+SELECT P.ProductName AS `Produto`, MIN(O.Quantity) AS `Mínima`, MAX(O.Quantity) AS `Máxima`, AVG(O.Quantity) AS `Média`
+FROM w3schools.order_details AS O
+INNER JOIN w3schools.products AS P
+ON P.ProductID = O.ProductID
+GROUP BY O.ProductID
+HAVING `Média` > 20 ORDER BY `Média` ASC, `Produto` ASC;
