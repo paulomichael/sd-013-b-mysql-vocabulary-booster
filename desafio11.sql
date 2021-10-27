6e2cd1af573cd8f532859f3ae85018618fbb8e7b
diogodlemos <diogodrummond@hotmail.com>
2021-10-26 21:48:30 -0300

    Desafios 11, 12 e 14 finalizados

---
+++ desafio11.sql
@@ -0,0 +1,12 @@
+SELECT 
+    ContactName AS `Nome`,
+    Country AS `País`,
+    (SELECT 
+            (COUNT(*) - 1)
+        FROM
+            w3schools.customers
+        WHERE
+            País = Country) AS `Número de compatriotas`
+FROM
+    w3schools.customers
+ORDER BY `Nome`;
---
+++ desafio13.sql
@@ -0,0 +1,9 @@
+SELECT 
+    p.ProductName AS `Produto`, p.Price AS `Preço`
+FROM
+    w3schools.products AS p
+        JOIN
+    w3schools.order_details AS od ON od.ProductID = p.ProductID
+WHERE
+    od.Quantity > 80
+ORDER BY `Produto`;
