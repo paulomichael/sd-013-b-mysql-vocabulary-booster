ad7ff9a6315e429085f21baff1d3073dfd083b72
Leonardo <leo_mc_inter@hotmail.com>
2021-10-26 23:10:22 -0300

    Requisito 11 novamente

--- desafio11.sql
+++ desafio11.sql
@@ -1,5 +1,4 @@
 SELECT
-    MAX(c1.CustomerID),
     c1.ContactName AS `Nome`,
     c1.Country AS 'País',
     COUNT(c1.Country) - 1 AS `Número de compatriotas`
@@ -8,6 +7,6 @@ FROM
     w3schools.customers AS c2
 WHERE
     c1.Country = c2.Country
-GROUP BY c1.ContactName, c1.Country
+GROUP BY c1.CustomerID
 HAVING `Número de compatriotas` > 1
 ORDER BY `Nome`;
