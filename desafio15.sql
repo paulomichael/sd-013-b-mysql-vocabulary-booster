de9733a2997bbcc3ae2d355e75e5442ec3c25466
Joao Victor Cristino F <jvictorfranca@yahoo.com.br>
2021-10-27 01:34:50 -0300

    corrige o 15 com round

--- desafio15.sql
+++ desafio15.sql
@@ -3,9 +3,9 @@ DELIMITER $$
 
 CREATE PROCEDURE buscar_media_por_cargo(IN job VARCHAR(50)) 
 BEGIN 
-SELECT AVG(t1.SALARY) AS  "Média salarial" FROM employees AS t1
+SELECT ROUND(AVG(t1.SALARY),2) AS  "Média salarial" FROM employees AS t1
 INNER JOIN jobs as t2
 ON t1.JOB_ID = t2.JOB_ID
 WHERE JOB_TITLE = job;
 END $$
-DELIMITER ; 
+DELIMITER ;
