10378fc8abad54faff3f026f06e5d131e7089b84
EddieRAM <edusacarneiro@gmail.com>
2021-10-27 10:20:09 -0300

    req 7 - solved ambiguous interpretation of the req

--- desafio7.sql
+++ desafio7.sql
@@ -1,9 +1,11 @@
 SELECT 
-    CONCAT(UCASE(FIRST_NAME), ' ', UCASE(LAST_NAME)) AS 'Nome completo',
-    HIRE_DATE AS 'Data de início',
-    SALARY AS 'Salário'
+    CONCAT(UCASE(e.FIRST_NAME), ' ', UCASE(e.LAST_NAME)) AS 'Nome completo',
+    jh.START_DATE AS 'Data de início',
+    e.SALARY AS 'Salário'
 FROM
-    hr.employees
+    hr.employees e
+    INNER JOIN job_history jh
+    ON e.EMPLOYEE_ID = jh.EMPLOYEE_ID
 WHERE
-    MONTH(HIRE_DATE) BETWEEN 1 AND 3
-ORDER BY CONCAT(FIRST_NAME, ' ', LAST_NAME), HIRE_DATE
+    MONTH(jh.START_DATE) BETWEEN 1 AND 3
+ORDER BY CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME), jh.START_DATE;
