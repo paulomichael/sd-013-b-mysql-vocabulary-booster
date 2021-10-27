f42ae7c7a8d6f7ad8d0c0263d69dab8e9346a944
RafaelReisTM <srp.rafael@gmail.com>
2021-10-27 11:00:05 -0300

    correcao "Nome de contato" do desafio8.sql

--- desafio8.sql
+++ desafio8.sql
@@ -1,9 +1,9 @@
 SELECT
-  CUSTOMERS.ContactName AS `Nome de Contato`,
+  CUSTOMERS.ContactName AS `Nome de contato`,
   SHIPPERS.ShipperName AS `Empresa que fez o envio`,
   ORDERS.OrderDate AS `Data do pedido`
 FROM
-  w3schools.orders AS ORDERS -- w3schools.customers AS CUSTOMERS
+  w3schools.orders AS ORDERS
   INNER JOIN w3schools.customers AS CUSTOMERS ON CUSTOMERS.CustomerID = ORDERS.CustomerID
   INNER JOIN w3schools.shippers AS SHIPPERS ON SHIPPERS.ShipperID = ORDERS.ShipperID
 WHERE
