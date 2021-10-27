2a1af8dc6d91dd86aa97173425400e073dfd3616
Lucas-L-S-Haine <lucaslshaine@gmail.com>
2021-10-27 13:35:59 -0300

    Desafio15.

---
+++ desafio15.sql
@@ -0,0 +1,12 @@
+DELIMITER $$
+CREATE PROCEDURE buscar_media_por_cargo(IN JOB VARCHAR(35))
+BEGIN
+SELECT
+ROUND(AVG(SALARY), 2) AS `Média salarial`
+FROM hr.employees AS E
+JOIN hr.jobs AS J
+ON E.JOB_ID = J.JOB_ID
+WHERE J.JOB_TITLE = JOB
+;
+END$$
+DELIMITER ;
