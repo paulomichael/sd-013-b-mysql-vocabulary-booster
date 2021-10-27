7c1e3bb051447ade7f1e79258cbf06b4efa2ed99
João Bruno Vasconcelos <joaobruno91@gmail.com>
2021-10-26 22:26:45 -0300

    Adiciona desafio 5 novamente

--- desafio5.sql
+++ desafio5.sql
@@ -1,10 +1,8 @@
 SELECT
-  jo.JOB_TITLE AS 'Cargo',
-  (jo.MAX_SALARY - jo.MIN_SALARY) AS 'Variação Salarial',
-  MIN(em.SALARY) AS 'Média mínima mensal',
-  MAX(em.SALARY) AS 'Média máxima mensal'
-FROM hr.jobs AS jo
-INNER JOIN hr.employees AS em
-ON jo.JOB_ID = em.JOB_ID
-GROUP BY jo.JOB_ID
+  JOB_TITLE AS 'Cargo',
+  (MAX_SALARY - MIN_SALARY) AS 'Variação Salarial',
+  ROUND(MIN(MIN_SALARY/12), 2) AS 'Média mínima mensal',
+  ROUND(MAX(MAX_SALARY/12), 2) AS 'Média máxima mensal'
+FROM hr.jobs
+GROUP BY JOB_ID 
 ORDER BY `Variação Salarial`, `Cargo`;
