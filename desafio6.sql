498f1c12d55d654efb2f4164c9f51a96d916fef6
João Bruno Vasconcelos <joaobruno91@gmail.com>
2021-10-26 23:30:27 -0300

    Adiciona desafio 6 do jeito correto

--- desafio6.sql
+++ desafio6.sql
@@ -1,10 +1,12 @@
 SELECT 
     CONCAT(em.FIRST_NAME, ' ', em.LAST_NAME) AS 'Nome completo',
     jo.JOB_TITLE AS 'Cargo',
-    em.HIRE_DATE AS 'Data de início do cargo',
+    jh.START_DATE AS 'Data de início do cargo',
     de.DEPARTMENT_NAME AS 'Departamento'
 FROM
     hr.employees AS em
+		INNER JOIN
+  	hr.job_history AS jh ON jh.EMPLOYEE_ID = em.EMPLOYEE_ID
         INNER JOIN
     hr.jobs AS jo ON em.JOB_ID = jo.JOB_ID
         INNER JOIN
