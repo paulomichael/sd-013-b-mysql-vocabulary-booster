bcf988e02f52ad80b4178490f035593e4397521c
Ygor Lage <75700712+Yg-or@users.noreply.github.com>
2021-10-27 19:46:18 -0300

    requisito 11

--- desafio11.sql
+++ desafio11.sql
@@ -1,6 +1,6 @@
 SELECT C.ContactName AS `Nome`, C.Country AS `País`, (COUNT(*) -1) AS `Número de compatriotas`
-FROM w3schools.customers AS C
-JOIN w3schools.customers AS D
-ON C.Country = D.Country GROUP BY `Nome`
+FROM w3schools.customers AS C,
+w3schools.customers AS D
+WHERE C.Country = D.Country GROUP BY `Nome`
 HAVING `Número de compatriotas` > 0
 ORDER BY `Nome`;
