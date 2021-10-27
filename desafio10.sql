0f086fb73c293424719e341f785680cfdde2c9bc
Ygor Lage <75700712+Yg-or@users.noreply.github.com>
2021-10-27 19:20:19 -0300

    requisito 10

--- desafio10.sql
+++ desafio10.sql
@@ -1,4 +1,4 @@
-SELECT P.ProductName AS `Produto`, MIN(O.Quantity) AS `Mínima`, MAX(O.Quantity) AS `Máxima`, AVG(O.Quantity) AS `Média`
+SELECT P.ProductName AS `Produto`, MIN(O.Quantity) AS `Mínima`, MAX(O.Quantity) AS `Máxima`, Round(AVG(O.Quantity), 2) AS `Média`
 FROM w3schools.order_details AS O
 INNER JOIN w3schools.products AS P
 ON P.ProductID = O.ProductID
