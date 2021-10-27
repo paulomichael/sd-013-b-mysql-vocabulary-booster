0b4fd8fc0bcff900ab229a13ed506bdb6dfc661d
Lopes <gus.lcampos38@gmail.com>
2021-10-27 13:46:55 -0300

    fix

--- desafio10.sql
+++ desafio10.sql
@@ -2,9 +2,9 @@
   
 SELECT 
     p.ProductName AS `Produto`,
-    MIN(od.Quantity) AS Mínima,
-    MAX(od.Quantity) AS Máxima,
-    ROUND(AVG(od.Quantity), 2) AS Média
+    MIN(od.Quantity) AS `Mínima`,
+    MAX(od.Quantity) AS `Máxima`,
+    ROUND(AVG(od.Quantity), 2) AS `Média`
 FROM
     w3schools.order_details AS od
         JOIN
@@ -12,5 +12,5 @@ FROM
         JOIN
     w3schools.products AS p ON p.ProductID = od.ProductID
 GROUP BY Produto
-HAVING Média > 20
-ORDER BY Média , Produto;
+HAVING `Média` > 20
+ORDER BY `Média` , `Produto`;
