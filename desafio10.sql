1f80e49f802da17df0dd101d20e510325e14d4ef
Dororops <leeurw@hotmail.com>
2021-10-27 03:59:57 -0300

    Arrumando req10 e 11

--- desafio10.sql
+++ desafio10.sql
@@ -1,7 +1,7 @@
 SELECT ProductName AS 'Produto',
 MIN(OD.Quantity) AS 'Mínima',
 MAX(OD.Quantity) AS 'Máxima',
-AVG(OD.Quantity) AS 'Média'
+round(AVG(OD.Quantity), 2) AS 'Média'
 FROM w3schools.products AS P
 JOIN w3schools.order_details AS OD ON P.ProductID = OD.ProductID
 GROUP BY ProductName
--- desafio11.sql
+++ desafio11.sql
@@ -7,5 +7,5 @@ w3schools.customers AS C1,
 w3schools.customers AS C2
 WHERE C1.Country = C2.Country
 AND C1.ContactName <> C2.ContactName
-GROUP BY `Nome`
+GROUP BY `Nome`, `País`
 ORDER BY `Nome`;
